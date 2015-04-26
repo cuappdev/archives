4cb033bd0242fedc8ef1db75e5e9b6917700a00c
Feifan Zhou <feifan@me.com>
2015-04-26 14:01:01 -0400

    Fill in all user props when creating user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,18 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_or_create_by(email: params[:user][:email])
+    @user = User.find_by(email: params[:user][:email])
     if @user
       # FIXME: Don't need to create a new session every time, update existing if possible
       @session = Session.create(user_id: @user.id)
     else
+      @user = User.create(user_params)
       @session = nil
     end
     render json: { success: !@session.blank?, user: @user, session: @session }
-  end 
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:fbid, :email, :name, :username)
+  end
 end
