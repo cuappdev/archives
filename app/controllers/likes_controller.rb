cb68446c38291d5f00b2a44dbfd8e31ad24ce832
Ilan Filonenko <ifilondz@gmail.com>
2015-04-26 14:56:45 -0400

    added post capability of like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,18 @@
 class LikesController < ApplicationController
+  def create
+    @like = Like.find_by(post_id: params[:like][:post_id])
+    if @like
+      # FIXME: Don't need to create a new session every time, update existing if possible
+      @message = 'Already liked'
+    else
+      @like = Like.create(like_params)
+      @message = 'Liked'
+    end
+    render json: { message: @message, like: @like }
+  end
+
+  private
+  def like_params
+    params.require(:like).permit(:user_id, :post_id)
+  end
 end
