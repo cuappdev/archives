11e9af5075ddb84ffb30a6c9736c3a09a5e06875
Ilan Filonenko <ifilondz@gmail.com>
2015-04-26 18:43:49 -0400

    like and follow takes objects

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,2 +1,24 @@
 class FollowingsController < ApplicationController
+  before_action :authorize, only: [:create]
+
+  def create
+    @follow = Following.find_by(follower_id: @user.id, followed_id: params[:following][:followed_id])
+    @unfollow = params[:following][:dislike]
+    if @follow && @unfollow
+      @user.unfollow(User.find_by(params[:following][:followers_id]))
+      @message = true
+    elsif @follow && !@unfollow
+      @message = false
+    else
+      @follow = Follow.create(follow_params)
+      @user.following(User.find_by(params[:following][:followers_id]))
+      @message = true
+    end
+    render json: { message: @message, follow: @follow }
+  end
+
+  private
+  def follow_params
+    params.require(:following).permit(:follower_id, :unfollow)
+  end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,13 +5,13 @@ class LikesController < ApplicationController
     @like = Like.find_by(post_id: params[:like][:post_id])
     @dislike = params[:like][:dislike]
     if @like && @dislike
-      @user.unlike(params[:like][:post_id])
+      @user.unlike(User.find_by(params[:like][:post_id]))
       @message = true
     elsif @like && !@dislike
       @message = false
     else
       @like = Like.create(like_params)
-      @user.like(params[:like][:post_id])
+      @user.like(User.find_by(params[:like][:post_id]))
       @message = true
     end
     render json: { message: @message, like: @like }
