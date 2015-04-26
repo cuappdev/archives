dcb3311f10f52e027a74f73adab321bfbbb31218
Ilan Filonenko <ifilondz@gmail.com>
2015-04-26 15:32:52 -0400

    simplified calls

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,13 +5,13 @@ class LikesController < ApplicationController
     @like = Like.find_by(post_id: params[:like][:post_id])
     @dislike = params[:like][:dislike]
     if @like && @dislike
-      @user.unlike(Post.find_by(id: params[:like][:post_id]))
+      @user.unlike(params[:like][:post_id])
       @message = true
     elsif @like && !@dislike
       @message = false
     else
       @like = Like.create(like_params)
-      @user.like(Post.find_by(id: params[:like][:post_id]))
+      @user.like(params[:like][:post_id])
       @message = true
     end
     render json: { message: @message, like: @like }
