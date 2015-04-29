4bc3fac5c9f5af89ca82228b3e9fd366aa312af8
Ilan Filonenko <ifilondz@gmail.com>
2015-04-29 19:03:30 -0400

    passing in song object

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,14 @@ class PostsController < ApplicationController
   def create
     user_id = @session.user_id
     @post = Post.create(user_id: user_id, like_count: 0)
-    @song = Song.create(spotify_url: params[:spotify_url])
+    @song = Song.create(song_params)
     SongPost.create(post_id: @post.id, song_id: @song.id)
     @success = (!@song.id.blank? and !@post.id.blank? and @post.songs.count==1)
     render json: { success: !@song.blank?, post: @post }
+  end
+
+  private 
+  def song_params
+    params.require(:song).permit(:artist, :track, :spotify_url)
   end
 end
