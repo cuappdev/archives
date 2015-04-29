ee09dd78cbe5a5c2652d77fde190a85681b5fd2b
Ilan Filonenko <ifilondz@gmail.com>
2015-04-29 18:58:10 -0400

    variable not corectly defined

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
     user_id = @session.user_id
     @post = Post.create(user_id: user_id, like_count: 0)
     @song = Song.create(spotify_url: params[:spotify_url])
-    SongPost.create(post_id: post.id, song_id: song.id)
+    SongPost.create(post_id: @post.id, song_id: @song.id)
     @success = (!@song.id.blank? and !@post.id.blank? and @post.songs.count==1)
     render json: { success: !@song.blank?, post: @post }
   end
