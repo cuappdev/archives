7408e00b1fd5eaba030c4c381ce8d795eb006264
Ilan Filonenko <ifilondz@gmail.com>
2015-04-29 18:56:29 -0400

    pass argument as a hash

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   def create
     user_id = @session.user_id
     @post = Post.create(user_id: user_id, like_count: 0)
-    @song = Song.create(params[:spotify_url])
+    @song = Song.create(spotify_url: params[:spotify_url])
     SongPost.create(post_id: post.id, song_id: song.id)
     @success = (!@song.id.blank? and !@post.id.blank? and @post.songs.count==1)
     render json: { success: !@song.blank?, post: @post }
