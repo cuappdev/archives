967dfda1a235ecd4a8092a6408eceb419a5d2439
Ilan Filonenko <ifilondz@gmail.com>
2015-04-29 18:47:05 -0400

    dont need params user_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authorize, only: [:create]
   def create
     user_id = @session.user_id
-    @post = Post.create(user_id: params[:user_id], like_count: 0)
+    @post = Post.create(user_id: user_id, like_count: 0)
     @song = Song.create(params[:spotify_url])
     SongPost.create(post_id: post.id, song_id: song.id)
     @success = (!@song.id.blank? and !@post.id.blank? and @post.songs.count==1)
