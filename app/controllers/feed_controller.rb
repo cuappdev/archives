31e5d41919cde8f0b86004b6676238ed48a797e9
Feifan Zhou <feifan@me.com>
2015-05-05 18:13:33 -0400

    Fix feed crash with too few following IDs

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,6 +1,8 @@
 class FeedController < ApplicationController
   before_action :authorize
   def index
-    render json: { posts: Post.where('created_at >= ?', Time.now.midnight).where('user_id in ?', @user.following.pluck(:id)).order('created_at DESC') }
+    following_ids = @user.following.pluck(:id)
+    following_ids = [following_ids] unless following_ids.is_a? Array
+    render json: { posts: Post.where('created_at >= ?', Time.now.midnight).where('user_id in ?', following_ids).order('created_at DESC') }
   end
 end
