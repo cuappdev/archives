f21e6a4690a3a404f4c28bda1d79a0ee6c3fbbff
Feifan Zhou <feifan@me.com>
2015-05-05 17:52:36 -0400

    Fix uniqueness validation on likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -12,6 +12,7 @@
 class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  validates :user_id, presence: true, unique: true
-  validates :post_id, presence: true, unique: true
+  validates :user_id, presence: true
+  validates :post_id, presence: true
+  validates_uniqueness_of :user_id, scope: :post_id
 end
