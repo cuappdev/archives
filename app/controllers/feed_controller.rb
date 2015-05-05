f4ed0e4aface835df22c7a2a4dab0cdbaf3a07c9
Feifan Zhou <feifan@me.com>
2015-05-05 18:04:32 -0400

    Limit feed#index to only show posts from people you follow

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,6 +1,6 @@
 class FeedController < ApplicationController
-  # before_action :authorize
+  before_action :authorize
   def index
-    render json: { posts: Post.where('created_at >= ?', Time.now.midnight).order('created_at DESC') }
+    render json: { posts: Post.where('created_at >= ?', Time.now.midnight).where('user_id in ?', @user.following.pluck(:id)).order('created_at DESC') }
   end
 end
