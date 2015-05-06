eca1b13296812f212eb6246bd9b98d958b31565f
Feifan Zhou <feifan@me.com>
2015-05-05 23:24:33 -0400

    Revert back to global feed to get things to work

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -3,6 +3,7 @@ class FeedController < ApplicationController
   def index
     following_ids = @user.following.pluck(:id)
     following_ids = [following_ids] unless following_ids.is_a? Array
-    render json: { posts: Post.where('created_at >= ?', Time.now.midnight).where('user_id in ?', following_ids).order('created_at DESC') }
+    # render json: { posts: Post.where('created_at >= ?', Time.now.midnight).where('user_id in ?', following_ids).order('created_at DESC') }
+    render json: { posts: Post.where('created_at >= ?', Time.now.midnight).order('created_at DESC') }
   end
 end
