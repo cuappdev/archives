e60fa0b3dedf4d8ec75de69d77c17f311055b6fc
Ilan Filonenko <ifilondz@gmail.com>
2015-11-17 15:44:20 -0500

    i am feeling do's more than each

--- app/helpers/followings_helper.rb
+++ app/helpers/followings_helper.rb
@@ -3,17 +3,17 @@ module FollowingsHelper
     mutual_followers = Following.where('followed_id = (?)',followed_id).pluck(:follower_id) - [user.id]
     return if mutual_followers.empty?
     if follow_bool
-      mutual_followers.each { |idx| 
+      mutual_followers.do |idx| 
         order = [user.id,idx].sort! {|x,y| x <=> y}
         relation = Mutualfriend.find_or_create_by(user1_id: order.first,user2_id: order.second)
         Mutualfriend.increment_counter(:mutual_friends_count, relation)
-      }
+      end
     else
-      mutual_followers.each { |idx|
+      mutual_followers.do |idx|
         order = [user.id,idx].sort! {|x,y| x <=> y}
         relation = Mutualfriend.find_by(user1_id: order.first, user2_id: order.second)
         Mutualfriend.decrement_counter(:mutual_friends_count, relation) if relation.mutual_friends_count != 0 
-      }
+      end
     end
   end
 end
