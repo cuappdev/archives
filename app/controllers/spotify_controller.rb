8cca97fd55adccf2e0c4dcd1008a9c8d318b28ce
Ilan Filonenko <ifilondz@gmail.com>
2015-12-02 21:20:03 -0500

    simplfy flow

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -1,30 +1,16 @@
 class SpotifyController < ApplicationController
   before_action :authorize, only: [:get_access_token]
-  def get_url
-    auth_url = client.auth_code.authorize_url(redirect_uri: redirect_uri, response_type: 'code', client_id: ENV["spotify_client_id"], state: params[:session_code])
-    render json: { success: true, url: auth_url}
-  end 
   def get_hash
     code = params[:code]
     session_code = params[:state]
     token = client.auth_code.get_token(params[:code], redirect_uri: redirect_uri).to_hash
-    p '--------'
-    p token
-    p '--------'
     @spotify_cred = SpotifyCred.create(user_id: Session.where(code: session_code).limit(1).pluck(:user_id).first,access_token: token[:access_token], refresh_token: token[:refresh_token], expires_at: token[:expires_at])
-    p @spotify_cred
     redirect_to "icefishing://access_token=#{token[:access_token]}"
-    # token.token
-    # token.refresh_token
-    # token.expires_at
-    # token = token.refresh!
-    # TODO: Save token.to_h
-    # TODO: Render
   end
   def get_access_token
     if @user.spotify_cred.blank?
-      render json: { error: "You didnt authorize with spotify yet"}
-      return
+      auth_url = client.auth_code.authorize_url(redirect_uri: redirect_uri, response_type: 'code', client_id: ENV["spotify_client_id"], state: params[:session_code])
+      render json: { success: false, url: auth_url} and return
     end
     creds = @user.spotify_cred
     token_hash = {
@@ -34,7 +20,7 @@ class SpotifyController < ApplicationController
     }
     access_token = OAuth2::AccessToken.from_hash(client, token_hash)
     access_token.refresh! if access_token.expired?
-    render json: { access_token: access_token.token }
+    render json: { success: true, access_token: access_token.token }
   end
 
   private
