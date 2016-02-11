0a9be7a4eec7ad45447e93df0d5aea14ebbe72b1
Ilan Filonenko <ifilondz@gmail.com>
2016-02-10 20:05:27 -0500

    Test

--- db/schema.rb
+++ db/schema.rb
@@ -82,7 +82,7 @@ ActiveRecord::Schema.define(version: 20151119011849) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.integer  "hipster_score"
+    t.integer  "hipster_score",    default: 0
     t.string   "caption"
     t.integer  "location_id"
     t.datetime "created_at",                   null: false
