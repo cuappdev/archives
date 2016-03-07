4160ebfaab8561ce144483bcbafd36d8da811b64
Jesse Chen <jmc622@cornell.edu>
2016-03-06 22:41:22 -0500

    current location button added and flip start and end point (issue 1 and 7)

--- The Cat/The Cat/Views/MapTextField.swift
+++ The Cat/The Cat/Views/MapTextField.swift
@@ -40,4 +40,11 @@ class MapTextField: UIView {
             d.endFieldSearch()
         }
     }
+
+    @IBAction func swapButtonPressed(sender: UIButton) {
+        let temp = endTextField.text
+        endTextField.text = startTextField.text
+        startTextField.text = temp
+        //TODO: center the map on the swapped start
+    }
 }
