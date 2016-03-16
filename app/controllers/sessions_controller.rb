e67d9c6c862316819fb382e531914320f16f0013
Celine Brass <cjb327@cornell.edu>
2016-03-16 18:38:58 -0400

    abstracted out facebook logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,36 +1,22 @@
 class SessionsController < ApplicationController
   def create
-    #####ADD facebook logic here#######
-    user_token = params[:user][:usertoken]
-    p user_token
-    p "**************************"
-    uri = URI.parse('https://graph.facebook.com/me?fields=id&access_token='+ user_token)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Get.new(uri.request_uri)
-
-    response = http.request(request)
-    p response.body
-    res = JSON.parse(response.body)
-    p res;
-    p res["id"]
-    fbid = res["id"]
-    params[:user][:fbid] = fbid
     #check for an existing user with this fbid
-    @user = User.find_by(fbid: fbid)
-    if @user
-      @session = Session.find_by(user_id:@user.id)
-    else
-      @user = User.create(user_params)
-      @session = Session.create(user_id: @user.id)
-    end
+    @user = User.find_or_create_by(user_params)
+    @session = Session.find_or_create_by(user_id: @user.id)
     render json: { success: !@session.blank?, user: @user, session: @session }
   end
 
   private
   def user_params
-    params.require(:user).permit(:email, :name, :username, :fbid)
+    user_token = params[:user][:usertoken]
+    uri = URI.parse('https://graph.facebook.com/me?fields=id&access_token='+ user_token)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    res = JSON.parse(response.body)
+    fbid = res["id"]
+    params.require(:user).permit(:email, :name, :username).merge(fbid: fbid)
   end
 end
