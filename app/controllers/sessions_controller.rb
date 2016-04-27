157249e6b10809abce9432095abf6353e4445ad2
Celine Brass <cjb327@cornell.edu>
2016-04-26 23:46:28 -0400

    get/post

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,7 +19,7 @@ class SessionsController < ApplicationController
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Get.new(uri.request_uri)
+    request = Net::HTTP.get.new(uri.request_uri)
     response = http.request(request)
     res = JSON.parse(response.body)
     p "THIS IS THE FACEBOOK RESPONSE"
