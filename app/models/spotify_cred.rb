49557c43fbdc8de18cdada4c5aa83a32519ff4bf
Celine Brass <cjb327@cornell.edu>
2016-04-26 23:16:37 -0400

    fixed more issues

--- app/models/spotify_cred.rb
+++ app/models/spotify_cred.rb
@@ -18,18 +18,12 @@ class SpotifyCred < ActiveRecord::Base
   validates :access_token, presence: true
   validates :refresh_token, presence: true
   validates :expires_at, presence: true
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> 52eb835b94149da674d82ff7891b3993f3456708
-  validates :spotify_id, presence: true
+  #validates :spotify_id, presence: true
 
   def update_playlist(playlist_id)
       self.playlist_id = playlist_id
       self.save
   end
-<<<<<<< HEAD
 
   def update_username(username)
       if (username)
@@ -38,7 +32,4 @@ class SpotifyCred < ActiveRecord::Base
       end
       return false
   end
->>>>>>> Stashed changes
-=======
->>>>>>> 52eb835b94149da674d82ff7891b3993f3456708
 end
