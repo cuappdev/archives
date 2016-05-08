a652cefa5ddc62cdd49318dc89a4a6a7dc5715f0
Ilan Filonenko <ifilondz@gmail.com>
2016-05-08 14:19:43 -0400

    fixed username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
   has_one :session
   has_one :spotify_cred
   validates :fbid, presence: true, uniqueness: {case_sensitive: false}
-
+  validates :username, uniqueness: {case_sensitive: false}
 
   # Follows a user
   def follow(followed_id)
