d285fa101d06b45f9a122baba815307e594209b1
Amit Mizrahi <amit@amizrahi.com>
2016-10-27 20:54:03 -0400

    Implement tests for feed, followings

--- spec/controllers/feed_controller_spec.rb
+++ spec/controllers/feed_controller_spec.rb
@@ -1,20 +1,29 @@
 require 'rails_helper'
+require './helpers'
 
 RSpec.describe FeedController, type: :controller do
 
-end
-
-"""
-class FeedController < ApplicationController
-  before_action :authorize, only: [:index]
-  def index
-    followings_ids = @user.followings_ids
-    posts = Post
-      .where(created_at: (Time.now - 24.hours)..Time.now)
-      .where('user_id IN (?)', followings_ids + [@user.id])
-      .order('created_at DESC')
-      .map { |post| post.as_json(id: @user.id) }
-    render json: { posts: posts }
+  before(:each) do
+    @user1 = FactoryGirl.create(:user, fbid: "bogus", username: "valid")
+    @user2 = FactoryGirl.create(:user, fbid: "fake", username: "alsovalid")
+    @user3 = FactoryGirl.create(:user, fbid: "something", username: "test")
+    @post = FactoryGirl.create(:post, like_count: 0, user_id: @user2.id)
+    @f = FactoryGirl.create(:following, follower_id: @user1.id, followed_id: @user2.id)
   end
+
+  it "grabs empty feed properly" do
+    # Grab feed when authenticated as @user3
+    get :feed
+    response_json = extract_response(response)
+    expect(response_json["posts"]).to eq([])
+  end
+
+  it "grabs feed items properly" do
+    # Grab feed when authenticated as user1, who is following user2
+    get :feed
+    r = { response: response, print: true, success: true }
+    response_json = extract_response(response)
+    expect(response_json["posts"]).to eq([@post.as_json(id: @user1.id)])
+  end
+
 end
-"""
--- spec/controllers/followings_controller_spec.rb
+++ spec/controllers/followings_controller_spec.rb
@@ -1,39 +1,44 @@
 require 'rails_helper'
+require './helpers'
 
 RSpec.describe FollowingsController, type: :controller do
 
-end
-
-"""
-# == Schema Information
-#
-# Table name: followings
-#
-#  id          :integer          not null, primary key
-#  follower_id :integer
-#  followed_id :integer
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#
-
-class FollowingsController < ApplicationController
-  before_action :authorize, only: [:create]
-  include FollowingsHelper
-  def create
-    # Is following?
-    follow_bool = (params[:unfollow] == "0")
-    # following id
-    followed_id = params[:followed_id]
-    # cant follow yourself
-    if @user.id == followed_id
-      render json: { success: false, follow: follow_bool}
-    end
-    # success value on the follow or unfollow
-    success_val = (follow_bool ? @user.follow(followed_id) : @user.unfollow(followed_id))
-    # if successful update the mutual friends by passing in whether it was follow,
-    # the user who followed, and the follower
-    update_mutual_friends(follow_bool, @user.id, followed_id) if success_val
-    render json: { success: success_val, follow: follow_bool }
+  before(:each) do
+    @user1 = FactoryGirl.create(:user, fbid: "bogus", username: "valid")
+    @user2 = FactoryGirl.create(:user, fbid: "fake", username: "alsovalid")
   end
+
+  it "follows previously unfollowed user" do
+    post :followings, unfollow: "0", followed_id: @user2.id
+    response_json = extract_response(response)
+    expect(response_json["success"]).to eq(true)
+    expect(response_json["follow"]).to eq(true)
+  end
+
+  it "unfollows already followed user" do
+    @f = FactoryGirl.create(:following, follower_id: @user1.id, followed_id: @user2.id)
+    post :followings, unfollow: "1", followed_id: @user2.id
+    response_json = extract_response(response)
+    expect(response_json["success"]).to eq(true)
+  end
+
+  it "doesn't unfollow already unfollowed user" do
+    post :followings, unfollow: "1", followed_id: @user2.id
+    response_json = extract_response(response)
+    expect(response_json["success"]).to eq(false)
+  end
+
+  it "doesn't follow already followed user" do
+    @f = FactoryGirl.create(:following, follower_id: @user1.id, followed_id: @user2.id)
+    post :followings, unfollow: "0", followed_id: @user2.id
+    response_json = extract_response(response)
+    expect(response_json["success"]).to eq(false)
+  end
+
+  it "cannot follow yourself" do
+    post :followings, follower_id: @user1.id, followed_id: @user1.id
+    response_json = extract_response(response)
+    expect(response_json["success"]).to eq(false)
+  end
+
 end
-"""
