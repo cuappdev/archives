b23b83d20354cba173b5c5d0424c656147d149d8
Amit Mizrahi <amit@amizrahi.com>
2016-10-29 18:36:52 -0400

    Episode table

--- src/main/scala/org/cuappdev/podcast/models/db/EpisodeEntityTable.scala
+++ src/main/scala/org/cuappdev/podcast/models/db/EpisodeEntityTable.scala
@@ -12,6 +12,7 @@ import org.cuappdev.podcast.models.DBInfo
 import org.cuappdev.podcast.models.EpisodeFields
 import org.cuappdev.podcast.utils.DatabaseConfig
 
+
 // Table Entity
 trait EpisodeEntityTable extends DatabaseConfig {
 
@@ -23,10 +24,17 @@ trait EpisodeEntityTable extends DatabaseConfig {
     def id = column[Option[Long]]("id", O.PrimaryKey, O.AutoInc)
     def created_at = column[Timestamp]("created_at")
     def updated_at = column[Timestamp]("updated_at")
-    // Insert other fields here...
+    def audiosearch_id = column[Long]("audiosearch_id")
+    def title = column[String]("title")
+    def description = column[String]("description")
+    def audio_url = column[String]("audio_url")
+    def image_url = column[String]("image_url")
+    def series_id = column[Int]("series_id")
+    def series_foreign_key = foreignKey("series_foreign_key", series_id, SeriesEntityTable.series)(_.id)
 
     // Required conversions for reading / writing to / from the DB
-    def * = ((id, created_at, updated_at), (/* fields here */)).shaped <>
+    def * = ((id, created_at, updated_at),
+      (audiosearch_id, title, description, audio_url, image_url, series_id, series_foreign_key)).shaped <>
       ( { case (dbInfo, episodeFields) => EpisodeEntity(DBInfo.tupled.apply(dbInfo), EpisodeFields.apply(episodeFields)) },
         { e: EpisodeEntity =>
           def f1(p: DBInfo) = DBInfo.unapply(p).get
