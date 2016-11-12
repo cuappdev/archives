a83aac6f1e2d5b880b42be4217aeba88a818f621
Amit Mizrahi <amit@amizrahi.com>
2016-11-12 14:47:06 -0500

    Test shell script

---
+++ tools/test/requests.sh
@@ -0,0 +1,4 @@
+http GET http://localhost:9000/v1/users
+http GET http://localhost:9000/v1/episodes
+http POST http://localhost:9000/v1/episodes audiosearch_id=1 title='Hi' description='Hello' audio_url="http://example.com" image_url="http://example.com" series_id=0
+http OPTIONS http://localhost:9000/v1/episodes
