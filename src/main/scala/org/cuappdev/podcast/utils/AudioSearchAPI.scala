8216a1d21be8600a6dec9c08826c436c00eebf69
Jma353 <jma353@cornell.edu>
2016-11-13 15:36:32 -0500

    Start of tests / audiosearch API suite

---
+++ src/main/scala/org/cuappdev/podcast/utils/AudioSearchAPI.scala
@@ -0,0 +1,21 @@
+package org.cuappdev.podcast.utils
+
+import akka.http.scaladsl.model.HttpResponse
+import scalaj.http._
+
+/* Filled with AudioSearchAPI HTTP requests */
+class AudioSearchAPI (audiosearchAppId: String, audiosearchSecret: String) {
+
+  val baseUrl = "https://www.audiosear.ch"
+  val headers : Map[String, String] = Map("Authorization" -> ("Basic " + audiosearchAppId + ":" + audiosearchSecret),
+                    "Content-Type" -> "application/x-www-form-urlencoded")
+
+  val oAuthResponse = Http(baseUrl + "/oauth/token").param("grant_type", "client_credentials").headers(headers)
+}
+
+/* Singleton */
+object AudioSearchAPIInstance extends Config {
+
+  val instance = new AudioSearchAPI (audiosearchAppId, audiosearchSecret)
+
+}
---
+++ src/test/scala/AudioSearchAPITest.scala
@@ -0,0 +1,13 @@
+import org.scalatest._
+import org.cuappdev.podcast.utils.Config
+import org.cuappdev.podcast.utils.AudioSearchAPI
+
+/* Tests for AudioSearchAPI class  */
+class AudioSearchAPITest extends FlatSpec with Config {
+
+  it should "work" in {
+    // TODO: Grab environment variables in scala for instantiating the object
+  }
+
+
+}
