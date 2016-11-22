44678ee6b21b8183439957f24216610dffb7b99b
Amit Mizrahi <amit@amizrahi.com>
2016-11-22 09:37:33 -0500

    Create sessions service

---
+++ src/main/scala/org/cuappdev/podcast/services/SessionsService.scala
@@ -0,0 +1,86 @@
+package org.cuappdev.podcast.services
+
+import org.cuappdev.podcast.models.db.{SessionEntityTable, UserEntityTable}
+import org.cuappdev.podcast.models.{SessionEntity, SessionFields, SessionFactory, UserEntity}
+import org.cuappdev.podcast.utils.Config
+import org.cuappdev.podcast.services.UsersService
+
+import java.sql.Timestamp
+import org.joda.time.DateTime
+
+// Execution requirements
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+object SessionsService extends SessionsService
+
+case class SessionNotFoundException(msg: String) extends Exception(msg: String)
+case class SessionExpiredException(msg: String) extends Exception(msg: String)
+
+trait SessionsService extends SessionEntityTable with UserEntityTable with Config {
+
+  import driver.api._
+
+  /** Helper function for generating a session token. **/
+  def generateToken() : String = {
+    "<put something here>"
+  }
+
+  /** Helper function that creates an 'expires_at' time for a session. **/
+  def generateExpiresAt() : Timestamp = {
+    val ttl : Long = 300000;
+    new Timestamp(DateTime.now.getMillis + ttl)
+  }
+
+  /** Checks if the session is valid (i.e. not expired) **/
+  def checkSessionValid(session: Future[Option[SessionEntity]]) : Future[Boolean] = {
+    session.flatMap {
+      case Some(s) => Future.successful(s.fields.expires_at.getMillis > DateTime.now.getMillis)
+      case None => {
+        Future.failed(new SessionNotFoundException("Session not found"))
+      }
+    }
+  }
+
+  /** Grabs a user by the session's token (as long as the session is not expired) **/
+  def grabUserBySessionToken(token: String) : Future[UserEntity] = {
+    val session : Future[Option[SessionEntity]] = db.run(sessions.filter(_.fields.token === token).result.headOption)
+    val expired : Future[Boolean] = checkSessionValid(session)
+    expired.flatMap {
+      case true => {
+        val user : Future[Option[UserEntity]] = db.run(users.filter(_.id == session.user_id).result.headOption)
+        user.flatMap {
+          case Some(u) => Future.successful(u)
+          case None => Future.failed(new UserNotFoundException("User associated with this session not found."))
+        }
+      }
+      case false => {
+        Future.failed(new SessionExpiredException("Session expired."))
+      }
+    }
+  }
+
+  def generateSession(updateToken: String) : Future[SessionEntity] = {
+    val session : Future[Option[SessionEntity]] = db.run(sessions.filter(_.fields.update_token == updateToken).result.headOption)
+    session.flatMap {
+      case Some(s) => val session = SessionFactory.create(
+        new SessionFields(
+          generateToken(),
+          generateToken(),
+          generateExpiresAt(),
+          s.fields.user_id)
+        );
+      Future.successful(session)
+      case None => Future.failed(new SessionNotFoundException("Session with this update token not found."))
+    }
+  }
+
+  // Get all the series
+  def getSessions(): Future[Seq[SessionEntity]] = db.run(sessions.result)
+
+  // Get a series by ID
+  def getSessionByID(id: Long): Future[Option[SessionEntity]] = {
+    db.run(sessions.filter(_.id === id).result.headOption)
+  }
+
+}
