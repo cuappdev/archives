f820c8e870faf4d10c97ab470786c58ce207cf8c
Ji Hun Kim <jk2227@cornell.edu>
2016-11-24 03:10:54 -0500

    updated annotations

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,21 +2,21 @@
 #
 # Table name: users
 #
-#  id               :integer          not null, primary key
-#  name             :string
-#  hipster_score    :integer          default(0)
-#  caption          :string
-#  followers_count  :integer          default(0)
-#  location_id      :integer
-#  like_count       :integer          default(0)
-#  fbid             :string
-#  username         :string
-#  email            :string
-#  followings_count :integer          default(0)
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
-#  push_id          :string
-#  active           :boolean          default(TRUE)
+#  id                                :integer          not null, primary key
+#  name                              :string
+#  hipster_score                     :integer          default(0)
+#  caption                           :string
+#  followers_count                   :integer          default(0)
+#  location_id                       :integer
+#  like_count                        :integer          default(0)
+#  fbid                              :string
+#  username                          :string
+#  email                             :string
+#  followings_count                  :integer          default(0)
+#  created_at                        :datetime         not null
+#  updated_at                        :datetime         not null
+#  push_id                           :string
+#  remote_push_notifications_enabled :boolean          default(TRUE)
 #
 
 module UsersHelper
