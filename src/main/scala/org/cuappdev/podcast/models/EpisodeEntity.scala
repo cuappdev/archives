8da88a9efd9f5c6e83bcb86a8fc4f41abe75b9e6
Jma353 <jma353@cornell.edu>
2016-12-02 21:10:35 -0500

    Create series from audiosearch response

--- src/main/scala/org/cuappdev/podcast/models/EpisodeEntity.scala
+++ src/main/scala/org/cuappdev/podcast/models/EpisodeEntity.scala
@@ -3,8 +3,7 @@ package org.cuappdev.podcast.models
 import spray.json.{JsArray, JsNumber, JsString, JsValue}
 ;
 
-/* Factory and entity for Episode. */
-
+/** Factory and entity for Episode. **/
 case class EpisodeFields (audiosearch_id: Long,
                           title: String,
                           description: String,
@@ -17,29 +16,33 @@ case class EpisodeEntity (dBInfo: DBInfo,
 
 object EpisodeFactory extends EntityFactory[EpisodeEntity, EpisodeFields] {
 
-  /* Necessary for JSON serialization */
+  /** Necessary for JSON serialization **/
   def instantiate(dbInfo: DBInfo, newFields: EpisodeFields) = {
     new EpisodeEntity(dbInfo, newFields)
   }
 
-  /* Creation from fields */
+  /** Creation from fields **/
   def create (f: EpisodeFields) : EpisodeEntity = {
     new EpisodeEntity(DBInfoFactory.create(), f)
   }
 
-  /* From JSON from audiosear.ch */
+  /** From JSON from audiosear.ch **/
   def create (jsonVal: JsValue) : EpisodeEntity = {
     val json = jsonVal.asJsObject
     val audiosearchID = json.fields("id").asInstanceOf[JsNumber].value.longValue()
     val title = json.fields("title").asInstanceOf[JsString].value
-    val description = json.fields.get("description") match { case None => "" case Some(d) => d.asInstanceOf[JsString].value }
-    val audioURL = json.fields("audio_files").asInstanceOf[JsArray]
-      .elements.apply(0).asJsObject.fields("mp3").asInstanceOf[JsString].value
+    val description = json.fields.get("description") match
+      { case None => "" case Some(d) => d.asInstanceOf[JsString].value }
+    val audioArr = json.fields("audio_files").asInstanceOf[JsArray]
+    val audioURL =
+      if (audioArr.elements.nonEmpty)
+        audioArr.elements.apply(0).asJsObject.fields("mp3").asInstanceOf[JsString].value
+      else ""
     val imageURL = json.fields("image_urls").asJsObject.fields("full").asInstanceOf[JsString].value
     EpisodeFactory.create(EpisodeFields(audiosearchID, title, description, audioURL, imageURL, None))
   }
 
-  /* Updates on diff-checking */
+  /** Updates on diff-checking **/
   def update (e: EpisodeEntity, newFields: EpisodeFields) : EpisodeEntity = {
     new EpisodeEntity(DBInfoFactory.update(e.dBInfo), newFields)
   }
--- src/main/scala/org/cuappdev/podcast/models/SeriesEntity.scala
+++ src/main/scala/org/cuappdev/podcast/models/SeriesEntity.scala
@@ -1,6 +1,8 @@
 package org.cuappdev.podcast.models
 
-/* Factory and entity for Series. */
+import spray.json.{JsArray, JsNumber, JsString, JsValue}
+
+/** Factory and entity for Series. **/
 
 case class SeriesFields (audiosearch_id: Long,
                         title: String,
@@ -19,6 +21,23 @@ object SeriesFactory extends EntityFactory[SeriesEntity, SeriesFields] {
   def create (f: SeriesFields) : SeriesEntity = {
     new SeriesEntity(DBInfoFactory.create(), f)
   }
+
+  def create (jsonVal : JsValue) : SeriesEntity = {
+    val json = jsonVal.asJsObject
+    val audiosearchID = json.fields("id").asInstanceOf[JsNumber].value.longValue()
+    val title = json.fields("title").asInstanceOf[JsString].value
+    val description = json.fields.get("description") match
+      { case None => "" case Some(d) => d.asInstanceOf[JsString].value }
+    val audioURL = json.fields("audio_files").asInstanceOf[JsArray]
+      .elements.apply(0).asJsObject.fields("mp3").asInstanceOf[JsString].value
+    val imageArr = json.fields("image_urls").asInstanceOf[JsArray]
+    val imageURL =
+      if (imageArr.elements.nonEmpty) imageArr.elements.apply(0)
+        .asJsObject.fields("file").asJsObject.fields("url").asInstanceOf[JsString].value
+      else  ""
+    SeriesFactory.create(SeriesFields(audiosearchID, title, description, imageURL))
+  }
+
 
   def update (e: SeriesEntity, newFields: SeriesFields) : SeriesEntity = {
     new SeriesEntity(DBInfoFactory.update(e.dBInfo), newFields)
