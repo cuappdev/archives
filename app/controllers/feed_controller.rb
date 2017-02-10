7fbfc2d4d2942a437b624fe007e25d769ef6c532
Ji Hun Kim <jk2227@cornell.edu>
2017-02-10 02:28:50 -0500

    testing out just returning other people's posts

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,7 +1,7 @@
 class FeedController < ApplicationController
   before_action :authorize, only: [:index]
   def index
-    queryResult = Post.joins("INNER JOIN followings ON (followings.follower_id = #{@user.id} AND posts.user_id = followings.followed_id) WHERE posts.created_at >= NOW() - '1 day'::INTERVAL UNION SELECT * FROM posts WHERE posts.user_id = #{@user.id} AND WHERE posts.created_at >= NOW() - '1 day'::INTERVAL").select("posts.*").order("posts.created_at DESC");
+    queryResult = Post.joins("INNER JOIN followings ON (followings.follower_id = #{@user.id} AND posts.user_id = followings.followed_id) WHERE posts.created_at >= NOW() - '1 day'::INTERVAL ORDER BY posts.created_at DESC").select("posts.*");
     posts = queryResult.map { |post| post.as_json(id: @user.id) }
     render json: { "posts": posts}
   end
