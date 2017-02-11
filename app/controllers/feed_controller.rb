56f916eb53ec341baf88cef3aac6ecdf9699f652
Ji Hun Kim <jk2227@cornell.edu>
2017-02-11 03:22:52 -0500

    trying out find by sql

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,7 +1,8 @@
 class FeedController < ApplicationController
   before_action :authorize, only: [:index]
   def index
-    queryResult = Post.joins("INNER JOIN followings ON (followings.follower_id = #{@user.id} AND posts.user_id = followings.followed_id) WHERE posts.created_at >= NOW() - '1 day'::INTERVAL ORDER BY posts.created_at DESC").select("posts.*");
+  	sqlQuery = "(SELECT posts.* FROM posts INNER JOIN followings ON (followings.follower_id = %i AND posts.user_id = followings.followed_id) WHERE posts.created_at >= NOW() - '1 day'::INTERVAL UNION SELECT * FROM posts WHERE posts.user_id = %i AND posts.created_at >= NOW() - '1 day'::INTERVAL) ORDER BY created_at DESC;" % [@user.id, @user.id]
+    queryResult = Post.find_by_sql(sqlQuery);
     posts = queryResult.map { |post| post.as_json(id: @user.id) }
     render json: { "posts": posts}
   end
