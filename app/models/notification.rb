c76d6f0f03daed9013c13693bff7d1562028cedb
Ji Hun Kim <jk2227@cornell.edu>
2017-02-21 22:45:08 -0500

    preparing for cron job

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -15,9 +15,29 @@ class Notification < ActiveRecord::Base
 	validates :from, presence: true
 	validates :to, presence: true
 	validates :notification_type, presence: true
+	include HttpHelper
 
   def seenNotification()
     self.seen = true 
     self.save
+  end
+
+  def self.notifyInactiveUsers(num_days)
+  	inactiveUsers = User.where('last_active < :x_days_ago', 
+    	:x_days_ago => DateTime.now - num_days.days)
+  	inactiveUsersToNotify = inactiveUsers.where(:remote_push_notifications_enabled => true)
+  	defaultMsg = "Seems like you have been inactive for a couple days... Come back and share a song or two! :)"
+  	inactiveUsersToNotify.each do |user|
+  		#### TODO: remove duplicate code ####
+  		sqlQuery = "(SELECT posts.* FROM posts INNER JOIN followings ON (followings.follower_id = %i AND posts.user_id = followings.followed_id) WHERE posts.created_at >= NOW() - '1 day'::INTERVAL UNION SELECT * FROM posts WHERE posts.user_id = %i AND posts.created_at >= NOW() - '1 day'::INTERVAL) ORDER BY created_at DESC;" % [user.id, user.id]
+  		queryResult = Post.find_by_sql(sqlQuery)
+  		#### above gets the posts in your current feed #### 
+  		msg = defaultMsg
+  		if queryResult.length > 0 
+  			msg = "You have #{queryResult.length} new song suggestions from your friends on Tempo!"
+  		end
+  		Notification.create(to: user.id, notification_type: 100, message: msg)
+  	    LikesController.helpers.notify([user.push_id], msg, 100)
+  	end
   end
 end 
