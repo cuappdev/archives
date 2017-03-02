68f18c60ec8ab7d3e304606ec6a63d2a5f6b7cee
Daniel Li <dl743@cornell.edu>
2017-03-01 21:17:40 -0500

    Add socket io and make a connection

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
+import http from 'http';
 import express from 'express';
 import path from 'path';
+import socket from 'socket.io';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8008;
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('*', (request, response) => {
   response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-})
+});
+
+const server = http.createServer(app);
+const io = socket(server);
+const socketPort = 3000;
+
+io.on('connection', (client) => {
+  console.log(`Client connected with id: ${client.conn.id}`);
+
+  client.on('event', (data) => {
+
+  });
+
+  client.on('disconnect', () => {
+    console.log(`Client disconnected with id: ${client.conn.id}`);
+  });
+});
+
+server.listen(socketPort, () => {
+  console.log ('Listening for socket.io connections on port ' + socketPort);
+});
 
 app.listen(port, () => {
-  console.log ("Clicker started on port " + port);
+  console.log('Started Clicker server on port ' + port);
 });
--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
+import io from 'socket.io-client';
+const socket = io(':3000');
+
 class PageLayout extends Component {
+
+  componentWillMount() {
+    socket.on('connect', () => {
+      console.log(`Connected to socket with id ${socket.id}`);
+    });
+  }
+
   render() {
     return (
       <div>
