c8545958cf88a2e4cd0548455d299e9b242104a5
Jma353 <jma353@cornell.edu>
2017-03-08 21:48:35 -0500

    Exception-handling

--- podcasts/storers/couchbase_storer.py
+++ podcasts/storers/couchbase_storer.py
@@ -10,10 +10,15 @@ class CouchbaseStorer(Storer):
 
   def __init__(self, url, password=None):
     """Constructor"""
-    self.url      = url # Bucket URL
-    self.password = password # Bucket password
-    self.lock     = threading.Lock() # Thread-safe utilization of this driver
-    self.db       = Bucket(self.url) if self.password is None else Bucket(self.url, password=self.password)
+    self.url        = url # Bucket URL
+    self.password   = password # Bucket password
+    self.lock       = threading.Lock() # Thread-safe utilization of this driver
+    self.db         = self._connect_db()
+    self.db.timeout = 10
+
+  def _connect_db(self):
+    """Connect the DB"""
+    return Bucket(self.url) if self.password is None else Bucket(self.url, password=self.password)
 
   def _make_series_key(self, series_dict):
     """Series key for Couchbase"""
@@ -28,6 +33,7 @@ class CouchbaseStorer(Storer):
   def store(self, result_dict):
     """See Storer#store(result_json)"""
     # Build properly formatted bulk insert
+    # plus batching
     bulk_upsert = dict()
     series_id = result_dict['series']['id']
     bulk_upsert[self._make_series_key(result_dict['series'])] = result_dict['series']
@@ -36,5 +42,13 @@ class CouchbaseStorer(Storer):
 
     # Bulk insert (thread-safe)
     self.lock.acquire()
-    self.db.upsert_multi(bulk_upsert)
+    # Ensure success
+    while True:
+      try:
+        self.db.upsert_multi(bulk_upsert)
+        bulk_upsert.clear()
+        break
+      except Exception:
+        print "WE GOT AN EXCEPTION BUT TRYING AGAIN"
+        self.db = self._connect_db()
     self.lock.release()
