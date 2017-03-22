649a76c81af008c1e3c46a232585cf936187d0a5
Ji Hun Kim <jk2227@cornell.edu>
2017-03-22 03:28:58 -0400

    pagination for notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -13,7 +13,11 @@
 
 class NotificationsController < ApplicationController
   def getNotifications
-    notifications = Notification.where(:to => params[:user_id]) 
+    page_length = params[:l].blank? ? 5 : (params[:l]).to_i
+    page = params[:p].blank? ? 0 : (params[:p]).to_i 
+    
+    notifications = Notification.where(:to => params[:user_id]).order('id DESC').slice(page * page_length, page_length).as_json
+
     render json: { notifications: notifications }
   end
 
