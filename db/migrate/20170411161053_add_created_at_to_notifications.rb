54e1378180f9546624f579edbcc6b5b4e421f56d
Ji Hun Kim <jk2227@cornell.edu>
2017-04-11 12:27:49 -0400

    use add_timestamps instead of manually adding in columns

--- db/migrate/20170411161053_add_created_at_to_notifications.rb
+++ db/migrate/20170411161053_add_created_at_to_notifications.rb
@@ -1,6 +1,7 @@
 class AddCreatedAtToNotifications < ActiveRecord::Migration
   def change
-    add_column :notifications, :created_at, :datetime, null:false
-    add_column :notifications, :updated_at, :datetime, null:false
+    #add_column :notifications, :created_at, :datetime, null:false
+    #add_column :notifications, :updated_at, :datetime, null:false
+    add_timestamps(:notifications, null: false)
   end
 end
