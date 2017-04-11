2634947d68af583192572ac84c79fd8b8e488c69
Ji Hun Kim <jk2227@cornell.edu>
2017-04-11 13:03:44 -0400

    populating timefields for existing entries

--- db/migrate/20170411161053_add_created_at_to_notifications.rb
+++ db/migrate/20170411161053_add_created_at_to_notifications.rb
@@ -1,7 +1,12 @@
 class AddCreatedAtToNotifications < ActiveRecord::Migration
   def change
-    #add_column :notifications, :created_at, :datetime, null:false
-    #add_column :notifications, :updated_at, :datetime, null:false
-    add_timestamps(:notifications, null: false)
+    add_timestamps(:notifications) 
+    Notification.find_each do |notification|
+      notification.created_at = DateTime.now
+      notification.updated_at = DateTime.now 
+      notification.save 
+    end
+    change_column_null :notifications, :created_at, false 
+    change_column_null :notifications, :updated_at, false
   end
 end
