8abbbe54a957b22ecd89301d6d008f9b02c5b0dd
Jma353 <jma353@cornell.edu>
2017-05-02 22:58:52 -0400

    Minor method name-changes and such

--- podcasts/models/episode.py
+++ podcasts/models/episode.py
@@ -9,13 +9,13 @@ class Episode(Entity):
     # Fill fields
     self.type         = 'episode'
     self.seriesId     = series.id
-    self.seriesTitle  = series.title # Already encoded
+    self.seriesTitle  = series.title      # Already encoded
     self.imageUrlSm   = series.imageUrlSm # Already encoded
     self.imageUrlLg   = series.imageUrlLg # Already encoded
     self.title        = '' if 'title' not in entry else entry['title'].encode('utf-8')
     self.author       = '' if 'author' not in entry else entry['author'].encode('utf-8')
     self.summary      = '' if 'summary_detail' not in entry else entry['summary_detail']['value'].encode('utf-8')
-    self.pubDate      = '' if 'published_parsed' not in entry else self._build_date_str(entry['published_parsed'])
+    self.pubDate      = '' if 'published_parsed' not in entry else self._build_date(entry['published_parsed'])
     self.duration     = '' if 'itunes_duration' not in entry else entry['itunes_duration'].encode('utf-8')
     self.tags         = [] if 'tags' not in entry else [(t['term'] if t['term'] is None else t['term'].encode('utf-8')) for t in entry['tags']]
 
--- podcasts/models/series.py
+++ podcasts/models/series.py
@@ -8,7 +8,6 @@ class Series(Entity):
   fields = ['id', 'title', 'country', 'author', 'imageUrlSm',
             'imageUrlLg', 'feedUrl', 'genres']
 
-
   def __init__(self, s_id, title, country, author, image_url_sm, image_url_lg, feed_url, genres):
     """
     Constructor -
@@ -24,30 +23,34 @@ class Series(Entity):
     self.feedUrl      = feed_url.encode('utf-8')
     self.genres       = genres.encode('utf-8')
 
-
   def __hash__(self):
-    """Series ID defines uniqueness"""
+    """
+    Series ID defines uniqueness
+    """
     return self.id
-
 
   @classmethod
   def from_json(cls, J):
-    """Series from iTunes JSON `J`"""
+    """
+    Series from iTunes JSON `J`
+    """
     return cls(J['collectionId'], J['collectionName'], J['country'],
                J['artistName'], J['artworkUrl60'], J['artworkUrl600'],
                J['feedUrl'], ';'.join(J['genres']))
 
-
   @classmethod
   def from_line(cls, L):
-    """Series from CSV line `L`"""
+    """
+    Series from CSV line `L`
+    """
     return cls(int(L['id'].decode('utf-8')), L['title'].decode('utf-8'),
                    L['country'].decode('utf-8'), L['author'].decode('utf-8'),
                    L['imageUrlSm'].decode('utf-8'), L['imageUrlLg'].decode('utf-8'),
                    L['feedUrl'].decode('utf-8'), L['genres'].decode('utf-8'))
 
-
   def to_line(self):
-    """To 'line' (a.k.a. array we can write with csv module)"""
+    """
+    To 'line' (a.k.a. array we can write with csv module)
+    """
     my_dict = self.__dict__
     return [my_dict[f] for f in Series.fields]
