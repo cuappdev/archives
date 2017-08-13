746496d9c6d97c7c2686b9f78e2de3525827b7aa
Jma353 <jma353@cornell.edu>
2017-08-13 15:49:58 -0700

    Comments on repo

--- server/src/repos/UsersRepo.js
+++ server/src/repos/UsersRepo.js
@@ -6,6 +6,7 @@ import User from '../models/User';
 import appDevUtils from '../utils/appDevUtils';
 import pool from './pool';
 
+// Create a user with fields
 const createUser = async (fields: UserFields): Promise<User> => {
   const user = new User(fields);
   const insertStatement = appDevUtils.insertIntoMySQLStatement(
@@ -19,16 +20,19 @@ const createUser = async (fields: UserFields): Promise<User> => {
   return createdUser;
 };
 
+// Get a user by Id
 const getUserById = async (id: number): Promise<?User> => {
   const rows = await pool(`SELECT * FROM users WHERE id=${id}`);
   return rows.length > 0 ? new User(rows[0]) : null;
 };
 
+// Get a user by googleId (a.k.a. unique key of their Google account)
 const getUserByGoogleId = async (googleId: string): Promise<?User> => {
   const rows = await pool(`SELECT * FROM users WHERE googleId='${googleId}'`);
   return rows.length > 0 ? new User(rows[0]) : null;
 };
 
+// Get users
 const getUsers = async (): Promise<Array<Object>> => {
   const rows = await pool('SELECT * FROM users');
   return rows;
