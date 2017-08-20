0603eaac2fae091af6ac9372d6d3af09fe27e3ac
Jma353 <jma353@cornell.edu>
2017-08-20 10:30:26 -0700

    Some models

---
+++ src/app/tempo/models/following.py
@@ -0,0 +1,14 @@
+from . import *
+from sqlalchemy import UniqueConstraint
+
+class Following(Base):
+  __tablename__ = 'followings'
+  __table_args__ = UniqueConstraint('follower_id', 'followed_id')
+  
+  id = db.Column(db.Integer, primary_key = True)
+  follower_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete = 'CASCADE'))
+  followed_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete = 'CASCADE'))
+
+  def __init__(self, **kwargs):
+    self.follower_id = kwargs.get('follower_id', 0)
+    self.followed_id = kwargs.get('followed_id', 0)
---
+++ src/app/tempo/models/like.py
@@ -0,0 +1,9 @@
+from . import *
+
+class Like(Base):
+  __tablename__ = 'likes'
+  __table_args__ = UniqueConstraint('post_id', 'user_id')
+  
+  id = db.Column(db.Integer, primary_key = True)
+  post_id = db.Column(db.Integer, db.ForeignKey('posts.id', ondelete = 'CASCADE'))
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete = 'CASCADE'))
