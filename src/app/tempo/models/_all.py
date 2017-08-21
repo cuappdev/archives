31dd126e142bed319c5a82ef3250f92e719156df
Jma353 <jma353@cornell.edu>
2017-08-20 21:19:40 -0400

    All schemas

--- src/app/tempo/models/_all.py
+++ src/app/tempo/models/_all.py
@@ -8,4 +8,38 @@ from song import *
 from spotify_cred import *
 from user import *
 
-# TODO - all schema
+class FollowingSchema(ModelSchema):
+  class Meta(ModelSchema.Meta):
+    model = Following
+
+class LikeSchema(ModelSchema):
+  class Meta(ModelSchema.Meta):
+    model = Like
+
+class NotificationSchema(ModelSchema):
+  class Meta(ModelSchema.Meta):
+    model = Notification
+
+class PostSchema(ModelSchema):
+  class Meta(ModelSchema.Meta):
+    model = Post
+
+class SessionSchema(ModelSchema):
+  class Meta(ModelSchema.Meta):
+    model = Session
+
+class SongPostSchema(ModelSchema):
+  class Meta(ModelSchema.Meta):
+    model = SongPost
+
+class SongSchema(ModelSchema):
+  class Meta(ModelSchema.Meta):
+    model = Song
+
+class SpotifyCredSchema(ModelSchema):
+  class Meta(ModelSchema.Meta):
+    model = SpotifyCred
+
+class UserSchema(ModelSchema):
+  class Meta(ModelSchema.Meta):
+    model = User
