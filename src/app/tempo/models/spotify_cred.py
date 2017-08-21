4999bd2989c1379d79ccb907ee9b1c5250aae0ce
Jma353 <jma353@cornell.edu>
2017-08-20 21:09:05 -0400

    Finish models

---
+++ src/app/tempo/models/spotify_cred.py
@@ -0,0 +1,20 @@
+from . import *
+
+class SpotifyCred(Base):
+  __tablename__ = 'spotify_creds'
+
+  id = db.Column(db.Integer, primary_key = True)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete = 'CASCADE'))
+  access_token = db.Column(db.String, nullable = False)
+  refresh_token = db.Column(db.String, nullable = False)
+  expires_at = db.Column(db.String, nullable = False)
+  spotify_id = db.Column(db.String, nullable = False)
+  playlist_id = db.Column(db.String)
+
+  def __init__(self, **kwargs):
+    self.user_id = kwargs.get('user_id')
+    self.access_token = kwargs.get('access_token')
+    self.refresh_token = kwargs.get('refresh_token')
+    self.expires_at = kwargs.get('expires_at')
+    self.spotify_id = kwargs.get('spotify_id')
+    self.playlist_id = kwargs.get('playlist_id', None)
