7f92e480dd9deb70aea28c8d8ea1ba15e5e12694
Jma353 <jma353@cornell.edu>
2017-08-25 01:30:21 -0400

    Re-order controllers

--- src/app/tempo/__init__.py
+++ src/app/tempo/__init__.py
@@ -8,44 +8,44 @@ tempo = Blueprint('tempo', __name__, url_prefix = '')
 from models._all import *
 
 # Import all controllers
-from controllers.user_authentication_controller import *
+from controllers.create_like_controller import *
+from controllers.create_post_controller import *
+from controllers.delete_like_controller import *
+from controllers.delete_post_controller import *
 from controllers.get_feed_controller import *
-from controllers.get_user_suggestions_controller import *
-from controllers.update_user_username_controller import *
+from controllers.get_is_post_liked_controller import *
+from controllers.get_spotify_hash_controller import *
+from controllers.get_spotify_sign_in_uri_controller import *
+from controllers.get_user_by_id_controller import *
 from controllers.get_user_followers_controller import *
 from controllers.get_user_followings_controller import *
-from controllers.get_spotify_sign_in_uri_controller import *
-from controllers.get_spotify_hash_controller import *
-from controllers.search_users_controller import *
-from controllers.get_is_post_liked_controller import *
-from controllers.search_spotify_tracks_controller import *
-from controllers.delete_post_controller import *
-from controllers.get_user_by_id_controller import *
 from controllers.get_user_liked_posts_controller import *
-from controllers.create_post_controller import *
 from controllers.get_user_posts_controller import *
-from controllers.create_like_controller import *
-from controllers.delete_like_controller import *
+from controllers.get_user_suggestions_controller import *
+from controllers.search_spotify_tracks_controller import *
+from controllers.search_users_controller import *
+from controllers.update_user_username_controller import *
+from controllers.user_authentication_controller import *
 
 controllers = [
-  UserAuthenticationController(),
+  CreateLikeController(),
+  CreatePostController(),
+  DeleteLikeController(),
+  DeletePostController(),
   GetFeedController(),
-  GetUserSuggestionsController(),
-  UpdateUserUsernameController(),
+  GetIsPostLikedController(),
+  GetSpotifyHashController(),
+  GetSpotifySignInUriController(),
+  GetUserByIdController(),
   GetUserFollowersController(),
   GetUserFollowingsController(),
-  GetSpotifySignInUriController(),
-  GetSpotifyHashController(),
-  SearchUsersController(),
-  GetIsPostLikedController(),
-  SearchSpotifyTracksController(),
-  DeletePostController(),
-  GetUserByIdController(),
   GetUserLikedPostsController(),
-  CreatePostController(),
   GetUserPostsController(),
-  CreateLikeController(),
-  DeleteLikeController(),
+  GetUserSuggestionsController(),
+  SearchSpotifyTracksController(),
+  SearchUsersController(),
+  UpdateUserUsernameController(),
+  UserAuthenticationController(),
 ]
 
 # Setup all controllers
