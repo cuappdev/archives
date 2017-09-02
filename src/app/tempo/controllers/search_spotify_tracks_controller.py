df5ec65d946861bb7129863d75857ea4b00203f1
Jma353 <jma353@cornell.edu>
2017-09-01 21:58:00 -0400

    Save to song to spotify endpoint

--- src/app/tempo/controllers/search_spotify_tracks_controller.py
+++ src/app/tempo/controllers/search_spotify_tracks_controller.py
@@ -12,26 +12,26 @@ class SearchSpotifyTracksController(AppDevController):
   def content(self, **kwargs):
     # Info for client request to spotify
     uri = 'https://accounts.spotify.com/api/token'
-    payload = { 'grant_type': 'client_credentials', }
+    payload = {'grant_type': 'client_credentials'}
     headers = make_authorization_headers(
-      os.environ['SPOTIFY_CLIENT_ID'],
-      os.environ['SPOTIFY_SECRET']
+        os.environ['SPOTIFY_CLIENT_ID'],
+        os.environ['SPOTIFY_SECRET']
     )
 
     # Grab AccessToken
-    r = requests.post(uri, data = payload, headers = headers)
+    r = requests.post(uri, data=payload, headers=headers)
     access_token = r.json()['access_token']
 
     # Get data for search request
     base_search_uri = 'https://api.spotify.com/v1/search'
     query = request.args['q']
-    params = urlencode({ 'q': query, 'type': 'track' })
-    auth_headers = { 'Authorization': 'Bearer {}'.format(access_token) }
+    params = urlencode({'q': query, 'type': 'track'})
+    auth_headers = {'Authorization': 'Bearer {}'.format(access_token)}
     search_uri = '{}?{}'.format(base_search_uri, params)
 
     # Search
     results = requests.\
-      get(search_uri, headers = auth_headers).\
-      json()['tracks']['items']
+        get(search_uri, headers=auth_headers).\
+        json()['tracks']['items']
 
-    return { 'items': results }
+    return {'items': results}
