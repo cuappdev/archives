96e461e2c3e3ae059ad0a613b6280542d672ba33
Kenneth Li <kennethli.3470@gmail.com>
2017-09-20 19:01:48 -0400

    Removed charset

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ DB_USERNAME = os.environ['DB_USERNAME']
 DB_PASSWORD = os.environ['DB_PASSWORD']
 DB_HOST = os.environ['DB_HOST']
 DB_NAME = os.environ['DB_NAME']
-DB_URL = 'postgres://{}:{}@{}/{}?charset=utf8mb4'.format(
+DB_URL = 'postgres://{}:{}@{}/{}'.format(
     DB_USERNAME,
     DB_PASSWORD,
     DB_HOST,
