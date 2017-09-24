0bfed7480e8a729d00123e49527951a070454770
Young Kim <young-k@protonmail.com>
2017-09-24 14:36:30 -0400

    made changes from PR

--- src/app/eventregister/s/models/app.py
+++ src/app/eventregister/s/models/app.py
@@ -1,13 +1,12 @@
 from . import *
 
 class App(Base):
-    __tablename__ = 'apps'
+  __tablename__ = 'apps'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    secret_key = db.Column(db.String(255), unique=True, nullable=False)
-    event_types = db.relationship('EventType', backref='app')
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(255), nullable=False)
+  secret_key = db.Column(db.String(255), unique=True, nullable=False)
 
-    def __init__(self, **kwargs):
-        self.name = kwargs.get('name')
-        self.secret_key = hashlib.sha1(os.urandom(64)).hexdigest()
+  def __init__(self, **kwargs):
+    self.name = kwargs.get('name')
+    self.secret_key = hashlib.sha1(os.urandom(64)).hexdigest()
