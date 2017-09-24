9d280bdcfa976e32c880abf0168226c4e479eb60
Kenneth Li <kennethli.3470@gmail.com>
2017-09-24 00:58:26 -0400

    Created event_type and event models

---
+++ src/app/eventregister/s/models/event_type.py
@@ -0,0 +1,17 @@
+from . import *
+
+class EventType(Base):
+    __tablename__ = 'event_types'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    creator = db.Column(db.String(50))
+    app_id = db.Column(db.Integer, db.ForeignKey('app.id'))
+
+    fields_info = db.Column(db.JSON)
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name')
+        self.creator = kwargs.get('creator')
+        self.fields_info = kwargs.get('fields_info')
+        
