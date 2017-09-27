881b8a98c14a3c5bea4f1d21d5fccc448a18f07a
Kenneth Li <kennethli.3470@gmail.com>
2017-09-27 00:54:01 -0400

    Fixed indentation and slight bug

--- src/app/eventregister/s/models/event_type.py
+++ src/app/eventregister/s/models/event_type.py
@@ -1,17 +1,17 @@
 from . import *
 
 class EventType(Base):
-    __tablename__ = 'event_types'
+  __tablename__ = 'event_types'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    creator = db.Column(db.String(50))
-    app_id = db.Column(db.Integer, db.ForeignKey('app.id'))
+  id = db.Column(db.Integer, primary_key=True)
+  # TODO: Integrate user model
+  name = db.Column(db.String(50))
+  creator = db.Column(db.String(50))
+  app_id = db.Column(db.Integer, db.ForeignKey('app.id'))
+  fields_info = db.Column(db.JSON)
 
-    fields_info = db.Column(db.JSON)
-
-    def __init__(self, **kwargs):
-        self.name = kwargs.get('name')
-        self.creator = kwargs.get('creator')
-        self.fields_info = kwargs.get('fields_info')
-        
+  def __init__(self, **kwargs):
+    self.name = kwargs.get('name')
+    self.creator = kwargs.get('creator')
+    self.fields_info = kwargs.get('fields_info')
+    
