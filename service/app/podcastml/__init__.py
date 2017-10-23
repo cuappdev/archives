e3aa2dac16bbb065e68a9acdd7f7fdc9314e6a5e
kevingreer <kevingreer@me.com>
2017-10-22 22:23:17 -0400

    removed comment

--- service/app/podcastml/__init__.py
+++ service/app/podcastml/__init__.py
@@ -3,7 +3,6 @@ from app.podcastml.utils import redisConnector
 
 # Define a Blueprint for this module (mchat)
 podcastml = Blueprint('podcastml', __name__, url_prefix='/api/v1')
-# redisConn = redisConnector.RedisConn("podcastML", "host")
 
 # Import all controllers
 from app.podcastml.controllers.recommend_episodes_for_user_controller import *
