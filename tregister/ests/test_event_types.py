503257e73e11a6f1ca797797295f9b9713db2cf6
Young Kim <young-k@protonmail.com>
2017-10-29 14:23:20 -0400

    event type testing done

--- tregister/ests/test_event_types.py
+++ tregister/ests/test_event_types.py
@@ -8,5 +8,51 @@ from app.events.dao import event_types_dao as etd
 class EventTypesTestCase(TestCase):
   def setUp(self):
     super(EventTypesTestCase, self).setUp()
-    EventType.query.delete()
+    Application.query.delete() # event-types should be cascade-deleted
+    user1 = ud.get_user_by_email(constants.TEST_USER_EMAIL)
+    test_app = ad.create_app("app1", user1.id)[1]
     db_session_commit()
+    commit_model(test_app)
+
+  def test_event_type_creation(self):
+    test_user = ud.get_user_by_email(constants.TEST_USER_EMAIL)
+    test_app = ad.get_app_by_name("app1")
+    fields_info = {
+        "ex1": {"type": "str", "required": False, "default": "hello"}
+    }
+    etd.create_event_type( # testing valid event
+        test_app.id,
+        "test_event_type",
+        test_user.id,
+        fields_info
+    )
+    with self.assertRaises(Exception):
+      fields_info = {
+          "ex1": {"type": "Str", "required": False, "default": 5}
+      }
+      etd.create_event_type( # testing invalid event
+          test_app.id,
+          "test_incorrect_event",
+          test_user.id,
+          fields_info
+      )
+    with self.assertRaises(Exception):
+      fields_info = {
+          "ex1": {"type": "str", "required": 5, "default": 5}
+      }
+      etd.create_event_type( # testing invalid event
+          test_app.id,
+          "test_incorrect_event",
+          test_user.id,
+          fields_info
+      )
+    with self.assertRaises(Exception):
+      fields_info = {
+          "ex1": {"type": "str", "required": False, "default": 5}
+      }
+      etd.create_event_type( # testing invalid event
+          test_app.id,
+          "test_incorrect_event",
+          test_user.id,
+          fields_info
+      )
