7ad0578a40bec18f9f92e0247bd5453a753289a2
Young Kim <young-k@protonmail.com>
2017-10-29 23:57:58 -0400

    finished basic event testing

--- tregister/ests/test_events.py
+++ tregister/ests/test_events.py
@@ -1,10 +1,11 @@
+import datetime
 from flask import json
 from tests.test_case import *
 from app import constants
 from app.events.dao import users_dao as ud
 from app.events.dao import applications_dao as ad
 from app.events.dao import event_types_dao as etd
-from app.events.dao import event_dao as ed
+from app.events.dao import events_dao as ed
 
 class EventsTestCase(TestCase):
   def setUp(self):
@@ -18,12 +19,19 @@ class EventsTestCase(TestCase):
     test_event_type = etd.create_event_type( # testing valid event
         test_app.id,
         "test_event_type",
-        test_user.id,
+        user1.id,
         fields_info
-    )
+    )[1]
     db_session_commit()
     commit_model(test_app)
     commit_model(test_event_type)
 
   def test_event_creation(self):
-    pass
+    test_app = ad.get_app_by_name("app1")
+    test_event_type = etd.get_event_type_by_name(test_app.id, "test_event_type")
+    e1 = ed.create_event(
+        test_event_type.id,
+        {"ex1": "test"},
+        datetime.datetime.utcnow()
+    )
+    e2 = ed.create_event(test_event_type.id, {}, datetime.datetime.utcnow())
