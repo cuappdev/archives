e5261a2e5c9cd0c388bc20391336893a67505765
Serge-Olivier Amega <sergeolivieramega@gmail.com>
2017-11-19 13:50:56 -0500

    make tests work with server

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -2,8 +2,11 @@ import UIKit
 import XCTest
 import SwiftRegister
 
-let testAppSecret = "018c5653072942f43215be586c967ad82352b2c2"
-let testApiUrl = URL(string: "http://localhost:5000/api/")!
+//let testAppSecret = "018c5653072942f43215be586c967ad82352b2c2"
+//let testApiUrl = URL(string: "http://localhost:5000/api/")!
+
+let testAppSecret = "984229841cb1f5a1aad41ff0227f2aa2ef7c7ed8"
+let testApiUrl = URL(string: "http://52.54.98.130/api/")!
 
 struct AlphaPayload: Payload {
     static let eventName: String = "alpha"
