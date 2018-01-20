c8da08e8a64586a87a28852c254c3325aed4b08d
kevingreer <kevingreer@me.com>
2018-01-20 18:22:14 -0500

    add overall option in series for topic

--- service/src/app/podcastml/controllers/recommend_series_for_topic_controller.py
+++ service/src/app/podcastml/controllers/recommend_series_for_topic_controller.py
@@ -1,4 +1,7 @@
+from app.podcastml.itunes_top_series_fetcher import overall_top_id
 from . import *
+
+overall_top_url_key = 'all'
 
 class RecommendSeriesForTopicController(AppDevController):
 
@@ -11,5 +14,6 @@ class RecommendSeriesForTopicController(AppDevController):
   @authorize
   def content(self, **kwargs):
     topic_id = request.view_args['topic_id']
+    topic_id = overall_top_id if topic_id == overall_top_url_key else topic_id
     series_ids = series_for_topic_dao.get_series_list_for_topic(topic_id)
     return {'series_ids': series_ids}
--- service/tests/test_series_for_topic.py
+++ service/tests/test_series_for_topic.py
@@ -1,5 +1,6 @@
 from testcase import *
 from app.podcastml.dao import series_for_topic_dao
+from app.podcastml.itunes_top_series_fetcher import overall_top_id
 
 class SeriesForTopicTestCase(TestCase):
 
@@ -11,6 +12,8 @@ class SeriesForTopicTestCase(TestCase):
   def test_model_creation(self):
     series_for_topic_dao.generate_series_for_topics()
     self.assertTrue(SeriesForTopic.query.count() > 0)
+    response = self.get('/api/v1/series/topic/all/')
+    self.assertIsNotNone(json.loads(response.data)['data']['series_ids'])
 
   def test_endpoint(self):
     self.validate_model_retreival('/api/v1/series/topic', SeriesForTopic,
