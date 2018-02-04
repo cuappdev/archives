9e862cb9bec351965f93f90b47044da168c9adf3
Young Kim <young-k@protonmail.com>
2018-02-04 12:13:18 -0500

    added more security to controller

--- src/app/eventregister/s/controllers/reset_app_secret_key_controller.py
+++ src/app/eventregister/s/controllers/reset_app_secret_key_controller.py
@@ -9,7 +9,15 @@ class ResetAppSecretKeyController(AppDevController):
 
   @authorize_user
   def content(self, **kwargs):
-    data = request.get_json()
+    user = kwargs.get('user')
     app_id = request.view_args['app_id']
 
-    return {"secret_key": applications_dao.reset_secret_key(app_id)}
+    try:
+      if int(app_id) not in {app.id for app in \
+                             users_dao.get_user_apps(user.id)}:
+        raise Exception('User not authorized for this app.')
+
+      params = request.args
+      return {"secret_key": applications_dao.reset_secret_key(app_id)}
+    except ValueError:
+      raise Exception('Invalid app ID.')
