d355ed38ea1c9895619c2a603fff6659d138072e
williambaibai <williambaibai@gmail.com>
2018-02-27 18:06:02 -0500

    Created models for gyms and users

---
+++ src/app/classes/models/user.py
@@ -0,0 +1,24 @@
+from . import *
+
+class User(Base):
+  __tablename__ = 'users'
+
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(255), nullable=False, unique=True)
+  first_name = db.Column(db.String(255))
+  last_name = db.Column(db.String(255))
+  image_url = db.Column(db.String(1500))
+
+  def __init__(self, **kwargs):
+    self.email = kwargs.get('email')
+    self.first_name = kwargs.get('first_name')
+    self.last_name = kwargs.get('last_name')
+    self.image_url = kwargs.get('image_url')
+
+  def serialize(self):
+    return {
+        'id': self.id,
+        'email': self.email,
+        'first_name': self.first_name,
+        'last_name': self.last_name
+    }
