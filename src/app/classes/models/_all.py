f7125e50de0b9cf804ff04e8f1c912c37949b374
k09001726 <zulkhoja1@gmail.com>
2018-02-27 21:54:21 -0500

    Added GymClass model

--- src/app/classes/models/_all.py
+++ src/app/classes/models/_all.py
@@ -1,6 +1,7 @@
 from marshmallow_sqlalchemy import ModelSchema
 from app.classes.models.user import *
 from app.classes.models.gym import *
+from app.classes.models.gymclass import *
 
 class UserSchema(ModelSchema):
  class Meta(ModelSchema.Meta):
@@ -9,3 +10,7 @@ class UserSchema(ModelSchema):
 class GymSchema(ModelSchema):
  class Meta(ModelSchema.Meta):
    model = Gym
+
+class GymClassSchema(ModelSchema):
+ class Meta(ModelSchema.Meta):
+   model = GymClass
