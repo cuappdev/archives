7ecdaa0ed9e331c74801e4fb8987c191b78f2c26
williambaibai <williambaibai@gmail.com>
2018-03-01 22:51:15 -0500

    Added gymclassinstance.py and instructor.py

--- src/app/classes/models/_all.py
+++ src/app/classes/models/_all.py
@@ -2,6 +2,8 @@ from marshmallow_sqlalchemy import ModelSchema
 from app.classes.models.user import *
 from app.classes.models.gym import *
 from app.classes.models.gymclass import *
+from app.classes.models.instructor import *
+from app.classes.models.gymclassinstance import *
 
 class UserSchema(ModelSchema):
  class Meta(ModelSchema.Meta):
@@ -14,3 +16,11 @@ class GymSchema(ModelSchema):
 class GymClassSchema(ModelSchema):
  class Meta(ModelSchema.Meta):
    model = GymClass
+
+class InstructorSchema(ModelSchema):
+  class Meta(ModelSchema.Meta):
+    model = Instructor
+
+class GymClassInstance(ModelSchema):
+  class Meta(ModelSchema.Meta):
+    model = GymClassInstance
