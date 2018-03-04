808f28a77bb99e033656a1619dc8c23d9e80657e
sameerkhoja <sk2327@cornell.edu>
2018-03-04 14:11:40 -0500

    created users dao
    user dao files
    
    removed changed in env.template
    
    Created daos for gymclass, gymclassinstance
    
    Added dao method for getgymclassinstance by time, and also by class and time
    
    removed import datetime from dao files
    
    removed get_gym_class_by_name
    
    changed instructor to instructor_id for get method in gymclass dao
    
    Created get_gym_classs_by_name_loc_instuct method and used it for optional_gym_class
    
    fixed bug
    
    updated get class instances by class by implementing pagination using sqlalchemy's paginate method
    
    added check for gym class instance
    
    removed get_gym_class_instances_by_class_time method
    
    added docstring for get_gym_class_instances_by_time

---
+++ src/app/classes/dao/gymclass_dao.py
@@ -0,0 +1,35 @@
+from . import *
+
+def get_gym_class_by_id(gym_class_id):
+  return GymClass.query.filter(GymClass.id == gym_class_id).first()
+
+def get_gym_classes_by_gym(gym_id):
+  return GymClass.query.filter(GymClass.gym == gym_id).all()
+
+def get_gym_classs_by_name_loc_instuct(name, gym_id, instructor):
+  return GymClass.query.fiter(GymClass.name = name,
+                              GymClass.gym_id = gym_id,
+                              GymClass.instructor = instructor)
+
+def get_gym_classes_by_instructor(instructor_id):
+  return GymClass.query.filter(GymClass.instructor_id == instructor_id).all()
+
+def create_gym_class(name, gym_id, description='', instructor=''):
+  optional_gym_class = get_gym_class_by_name_loc_instruct(name, gym_id, instructor)
+
+  if optional_gym_class is not None:
+     return False, optional_gym_class
+
+  gym = gyms_dao.get_gym_by_id(gym_id)
+
+  if gym is None:
+    raise Exception('Gym does not exist.')
+
+  gym_class = GymClass(
+      gym_id=gym_id,
+      name=name,
+      description=description,
+      instructor=instructor
+  )
+  db_utils.commit_model(gym_class)
+  return True, gym_class
