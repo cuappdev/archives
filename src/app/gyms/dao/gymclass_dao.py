dfafa090b3e349ac398af147a3da8b49cd1204e1
k09001726 <zulkhoja1@gmail.com>
2018-03-07 18:37:57 -0500

    Fixed issue where I could not create a gym class instance

--- src/app/gyms/dao/gymclass_dao.py
+++ src/app/gyms/dao/gymclass_dao.py
@@ -6,10 +6,10 @@ def get_gym_class_by_id(gym_class_id):
 def get_gym_classes_by_gym(gym_id):
   return GymClass.query.filter(GymClass.gym == gym_id).all()
 
-def get_gym_classs_by_name_loc_instuct(name, gym_id, instructor):
-  return GymClass.query.fiter(GymClass.name = name,
-                              GymClass.gym_id = gym_id,
-                              GymClass.instructor = instructor)
+def get_gym_class_by_name_loc_instruct(name, gym_id, instructor_id):
+  return GymClass.query.filter(GymClass.name == name,
+                          GymClass.gym_id == gym_id,
+                          GymClass.instructor_id == instructor_id).first()
 
 def get_gym_classes_by_instructor(instructor_id):
   return GymClass.query.filter(GymClass.instructor_id == instructor_id).all()
@@ -29,7 +29,7 @@ def create_gym_class(name, gym_id, description='', instructor=''):
       gym_id=gym_id,
       name=name,
       description=description,
-      instructor=instructor
+      instructor_id=instructor_id
   )
   db_utils.commit_model(gym_class)
   return True, gym_class
