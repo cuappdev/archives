b73fb5d1eec79cf341ba3f82c5ce88c370e7a96f
k09001726 <zulkhoja1@gmail.com>
2018-03-07 11:44:59 -0500

    Created daos for gymclass, gymclassinstance

---
+++ src/app/classes/dao/gymclass_dao.py
@@ -0,0 +1,34 @@
+from . import *
+import datetime
+
+def get_gym_class_by_id(gym_class_id):
+  return GymClass.query.filter(GymClass.id == gym_class_id).first()
+
+def get_gym_class_by_name(gym_class_name):
+    return GymClass.query.filter(GymClass.name == gym_class_name).first()
+
+def get_gym_classes_by_gym(gym_id):
+  return GymClass.query.filter(GymClass.gym == gym_id).all()
+
+def get_gym_classes_by_instructor(instructor_name):
+  return GymClass.query.filter(GymClass.instructor == instructor_name).all()
+
+def create_gym_class(name, gym_id, description='', instructor=''):
+  optional_gym_class = get_gym_class_by_name(name)
+
+  if optional_gym_class is not None:
+     return False, optional_gym_class
+
+  gym = gyms_dao.get_gym_by_id(gym_id)
+
+  if gym is None:
+    raise Exception('Gym does not exist.')
+
+  gym_class = GymClass(
+      gym_id=gym_id,
+      name=name,
+      description=description,
+      instructor=instructor
+  )
+  db_utils.commit_model(gym_class)
+  return True, gym_class
---
+++ src/app/classes/dao/gymclassinstance_dao.py
@@ -0,0 +1,25 @@
+from . import *
+import datetime
+
+def get_gym_class_instance_by_id(gym_class_instance_id):
+  return GymClassInstance.query.filter(GymClassInstance.id == gym_class_instance_id).first()
+
+def get_gym_class_instances_by_gym_class(gym_class_name):
+    return GymClassInstance.query.filter(GymClassInstance.gym_class == gym_class_name).all()
+
+def get_gym_class_instances_not_cancelled():
+  return GymClassInstance.query.filter(GymClassInstance.is_cancelled == False).all()
+
+def create_gym_class_instance(gym_class_id, start_time, duration):
+  gym_class = gymclass_dao.get_gym_class_by_id(gym_class_id)
+
+  if gym_class is None:
+    raise Exception('Gym class does not exist.')
+
+  gym_class_instance = GymClassInstance(
+      gym_class_id=gym_class_id,
+      start_time=start_time,
+      duration=duration
+  )
+  db_utils.commit_model(gym_class_instance)
+  return True, gym_class_instance
