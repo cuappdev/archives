9a6c1fac2fd136c7983872d7ae19ad12773ae2f7
williambaibai <williambaibai@gmail.com>
2018-03-07 01:14:06 -0500

    fixed issues in PR

--- src/app/classes/dao/gymhours_dao.py
+++ src/app/classes/dao/gymhours_dao.py
@@ -1,20 +1,39 @@
 from . import *
 import datetime
 
-def get_gym_hour_by_id(gym_hours_id):
-  return GymHour.query.filter(GymHour.id == gym_hours_id).first()
+def get_gym_hour(params):
+  """Parameterized getter for gym_hours
 
-def get_gym_hour_by_day_of_week(gym_id, day_of_week):
-  return GymHour.query.filter(GymHour.gym_id == gym_id,
-                              GymHour.day_of_week == day_of_week).first()
+  params arguments:
+  "id" -- the id of gym_hour
+  "gym_id" -- the id of the gym of gym_hour
+  "day_of_week" -- int from 0 to 6
+  "time" -- datetime.time object
 
-def get_gym_hours_by_gym_id(gym_id):
-  return GymHour.query.filter(GymHour.gym_id == gym_id).all()
-
-def get_gym_hours_by_day_time(time, day_of_week):
-  return GymHour.query.filter(GymHour.start_time < time,
-                              GymHour.close_time > time,
-                              GymHours.day_of_week == day_of_week).all()
+  If params contain "id": gets gym_hour with id
+  If params contain "gym_id": gets all gym_hours associated with gym_id
+  If params contain "gym_id" and "day_of_week": gets gym_hour for gym_id on the
+    specficied day_of_week
+  If params contain "time" and "day_of_week": gets all gym_hours on day_of_week
+    for which time is between open_time and close_time
+  """
+  keys = params.keys()
+  if "id" in keys:
+    return GymHour.query.filter(GymHour.id == params["id"]).first()
+  if "gym_id" in keys:
+    if "day_of_week" in keys:
+      return GymHour.query.filter(GymHour.gym_id == params["gym_id"],
+                                  GymHour.day_of_week == params["day_of_week"]
+                                  ).first()
+    else:
+      return GymHour.query.filter(GymHour.gym_id == params["gym_id"]).all()
+  elif "time" in keys and "day_of_week" in keys:
+    return GymHour.query.filter(GymHour.start_time < params["time"],
+                                GymHour.close_time > params["time"],
+                                GymHour.day_of_week == params["day_of_week"]
+                                ).all()
+  else:
+    raise Exception('No valid parameters')
 
 def create_gym_hour(gym_id, day_of_week, open_time, close_time):
   optional_gym_hour = get_gym_hour_by_day_of_week(gym_id, day_of_week)
--- src/app/classes/dao/gyms_dao.py
+++ src/app/classes/dao/gyms_dao.py
@@ -4,25 +4,20 @@ import datetime
 def get_gym_by_id(gym_id):
   return Gym.query.filter(Gym.id == gym_id).first()
 
-def get_gym_by_name(gym_name):
-  return Gym.query.filter(Gym.name == gym_name).first()
-
 def get_gym_hours(gym_id):
-  hours = gymhours_dao.get_gym_hours_by_gym_id(gym_id)
+  hours = gymhours_dao.get_gym_hour({"id": gym_id})
 
   if hours is None:
-    raise Exception('gym_hours do not exist')
+    raise Exception('There are no gym_hours for this gym')
 
   return hours
 
 def get_open_gyms(time, day_of_week):
-  result = []
-  for gymhour in gymhours_dao.get_gym_hours_by_day_time(time, day_of_week):
-    result.append(gymhour.gym)
-  return result
+  hours = gymhours_dao.get_gym_hour({"time": time, "day_of_week": day_of_week})
+  return [h.gym for h in hours]
 
 def create_gym(name, equipment='', location=''):
-  optional_gym = get_gym_by_name(name)
+  optional_gym = Gym.query.filter(Gym.name == name).first()
 
   if optional_gym is not None:
     return False, optional_gym
