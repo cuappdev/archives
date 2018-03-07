0c679f1e2c4a642c456f2667be9bc313a839e01c
k09001726 <zulkhoja1@gmail.com>
2018-03-07 13:12:13 -0500

    added check for gym class instance

--- src/app/classes/dao/gymclassinstance_dao.py
+++ src/app/classes/dao/gymclassinstance_dao.py
@@ -6,6 +6,10 @@ def get_gym_class_instance_by_id(gym_class_instance_id):
 def get_gym_class_instances_by_gym_class(gym_class_name, page, page_size=10):
   return GymClassInstance.query.filter(GymClassInstance.gym_class == gym_class_name).paginate(page, page_size, False)
 
+def get_gym_class_by_start_duration(start_time, duration):
+  return GymClassInstance.query.filter(GymClassInstance.start_time == start_time,
+                                       GymClassInstance.duration == duration)
+
 def get_gym_class_instances_not_cancelled():
   return GymClassInstance.query.filter(GymClassInstance.is_cancelled == False).all()
 
@@ -19,6 +23,11 @@ def get_gym_class_instances_by_class_time(gym_class_name, time):
                                        GymClassInstance.start_time + GymClassInstance.duration > time).all()
 
 def create_gym_class_instance(gym_class_id, start_time, duration):
+  optional_gym_class_instance = get_gym_class_by_start_duration(start_time, duration)
+
+  if optional_gym_class_instance is not None:
+    return False, optional_gym_class_instance
+
   gym_class = gymclass_dao.get_gym_class_by_id(gym_class_id)
 
   if gym_class is None:
