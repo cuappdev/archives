417e6825cf1d2df1930ba29fcb2dcc18e4a75adc
k09001726 <zulkhoja1@gmail.com>
2018-03-07 12:31:57 -0500

    Added dao method for getgymclassinstance by time, and also by class and time

--- src/app/classes/dao/gymclassinstance_dao.py
+++ src/app/classes/dao/gymclassinstance_dao.py
@@ -5,10 +5,19 @@ def get_gym_class_instance_by_id(gym_class_instance_id):
   return GymClassInstance.query.filter(GymClassInstance.id == gym_class_instance_id).first()
 
 def get_gym_class_instances_by_gym_class(gym_class_name):
-    return GymClassInstance.query.filter(GymClassInstance.gym_class == gym_class_name).all()
+  return GymClassInstance.query.filter(GymClassInstance.gym_class == gym_class_name).all()
 
 def get_gym_class_instances_not_cancelled():
   return GymClassInstance.query.filter(GymClassInstance.is_cancelled == False).all()
+
+def get_gym_class_instances_by_time(time):
+  return GymClassInstance.query.filter(GymClassInstance.start_time < time,
+                                       GymClassInstance.start_time + GymClassInstance.duration > time).all()
+
+def get_gym_class_instances_by_class_time(gym_class_name, time):
+  return GymClassInstance.query.filter(GymClassInstance.gym_class = gym_class_name,
+                                       GymClassInstance.start_time < time,
+                                       GymClassInstance.start_time + GymClassInstance.duration > time).all()
 
 def create_gym_class_instance(gym_class_id, start_time, duration):
   gym_class = gymclass_dao.get_gym_class_by_id(gym_class_id)
