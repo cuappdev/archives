37c12d617991a331b9f8728bbffe82319d2f10f4
williambaibai <williambaibai@gmail.com>
2018-03-07 01:14:06 -0500

    fixed issues in PR

--- src/app/classes/dao/instructors_dao.py
+++ src/app/classes/dao/instructors_dao.py
@@ -6,11 +6,14 @@ def get_instructor_by_id(instructor_id):
 def get_instructor_by_email(email):
   return Instructor.query.filter(Instructor.email == email).first()
 
-def create_instructor(email, first_name, last_name=''):
+def create_instructor(email, first_name='', last_name=''):
   optional_instructor = get_instructor_by_email(email)
 
   if optional_instructor is not None:
     return False, optional_instructor
+
+  if first_name == '' and last_name == '':
+    raise Exception('Both first_name and last_name are not initialized')
 
   instructor = Instructor(
       first_name=first_name,
