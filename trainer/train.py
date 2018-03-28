5d28131291135ef696552558c806eb405b65504b
Ilan Filonenko <if56@cornell.edu>
2018-03-28 13:28:01 -0400

    updated trainer

--- trainer/train.py
+++ trainer/train.py
@@ -2,11 +2,13 @@
 
 import os
 from collections import defaultdict
+import sys
 
+from appdev.connectors import MySQLConnector
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
+from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
 from pyspark.mllib.linalg.distributed import CoordinateMatrix
-from appdev.connectors import MySQLConnector, RedisConnector
 
 entry_redis_key = 'training_entries'
 
@@ -17,39 +19,37 @@ weights = {
     'subscriptions': 0.2
 }
 
-def get_matrix_entries():
-  conn = MySQLConnector(os.environ['DB_USERNAME'],
-                        os.environ['DB_PASSWORD'],
-                        os.environ['DB_HOST'],
-                        os.environ['DB_NAME'])
+def get_matrix_entries(db_u,db_p,db_h,db_n1,db_n2):
+  conn = MySQLConnector(db_u,
+                        db_p,
+                        db_h,
+                        db_n1)
 
   # table name -> (user_id index, episode_id index)
-  db_tables = {
-      'recommendations': (3, 4),
-      'bookmarks': (3, 4),
-      'listening_histories': (4, 5)
-  }
+  db_tables = ['recommendations','bookmarks','listening_histories']
 
   matrix_entries = defaultdict(float)  # (user_id, episode_id) -> score
 
-  for table, (user_index, episode_index) in db_tables.iteritems():
+  for table in db_tables:
     rows = conn.read_batch(table)
     for row in rows:
-      user_id, episode_id = row[user_index], row[episode_index]
+      # unnecessary int() casting but just in case....
+      user_id, episode_id = int(row['user_id']), int(row['episode_id'])
       matrix_entries[(user_id, episode_id)] += weights[table]
 
   rows = conn.read_batch('subscriptions')
   series_to_users = defaultdict(list)
   for row in rows:
-    user_id, series_id = row[3], row[4]
+    # unnecessary int() casting but just in case....
+    user_id, series_id = int(row['user_id']), int(row['series_id'])
     series_to_users[series_id].append(user_id)
 
   conn.close()
 
-  conn_podcast = MySQLConnector(os.environ['DB_USERNAME'],
-                                os.environ['DB_PASSWORD'],
-                                os.environ['DB_HOST'],
-                                os.environ['PODCAST_DB_NAME'])
+  conn_podcast = MySQLConnector(db_u,
+                                db_p,
+                                db_h,
+                                db_n2)
 
   episode_query = 'SELECT id FROM episodes WHERE series_id={}'
 
@@ -57,22 +57,42 @@ def get_matrix_entries():
       [episode_query.format(series_id) for series_id in series_to_users])
   for (series_id, episode_list) in zip(series_to_users, rows):
     for row in episode_list:
-      episode_id = row[0]
+      # unnecessary int() casting but just in case....
+      episode_id = int(row['id'])
       for user_id in series_to_users[series_id]:
-        matrix_entries[(user_id, episode_id)] += 0.2
+        matrix_entries[(user_id, episode_id)] += weights['subscriptions']
 
   conn_podcast.close()
 
-  return [(u, i, s) for (u, i), s in matrix_entries.iteritems()]
+  return [Rating(u, i, s) for (u, i), s in matrix_entries.iteritems()]
 
 
 if __name__ == "__main__":
   sc = SparkContext()
   spark = SparkSession(sc)
-  redis = RedisConnector('entry_checkpoint')
-  connection = redis._single_connect()
-  entries = redis.get_dictionary(connection, entry_redis_key)
-  if entries is None:
-    entries = get_matrix_entries()
-    redis.dump_dictionary(connection, {entry_redis_key: entries})
-  mat = CoordinateMatrix(sc.parallelize(entries))
+  # redis = RedisConnector('entry_checkpoint')
+  # connection = redis._single_connect()
+  # entries = redis.get_dictionary(connection, entry_redis_key)
+  # if entries is None:
+  #   entries = get_matrix_entries()
+  #   redis.dump_dictionary(connection, {entry_redis_key: entries})
+  print "building ratings"
+  print sys.argv
+  ratings = sc.parallelize(
+      get_matrix_entries(sys.argv[1],
+                         sys.argv[2],
+                         sys.argv[3],
+                         sys.argv[4],
+                         sys.argv[5]))
+  print ratings.take(10)
+  print "built ratings"
+  rank = int(sys.argv[6])
+  numIterations = int(sys.argv[7])
+  model = ALS.train(ratings, rank, numIterations)
+  testdata = ratings.map(lambda p: (p[0], p[1]))
+  predictions = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
+  ratesAndPreds = ratings.map(lambda r: ((r[0], r[1]), r[2])).join(predictions)
+  MSE = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1])**2).mean()
+  print "Mean Squared Error = " + str(MSE)
+  recommendations = model.recommendProductsForUsers(int(sys.argv[8]))
+  print recommendations
