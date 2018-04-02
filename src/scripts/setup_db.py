4f07c88cb59a6d0935385a00eee2d9e730ec50bc
williambaibai <williambaibai@gmail.com>
2018-04-02 16:22:24 -0400

    hard coded gym_hours

--- src/scripts/setup_db.py
+++ src/scripts/setup_db.py
@@ -1,9 +1,11 @@
 import sys
 import os
 import shutil
+import datetime as dt
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app.dao import gyms_dao as gd
+from app.dao import gymhours_dao as ghd
 
 def setup_dbs():
   print 'Setting up databases...'
@@ -27,11 +29,42 @@ def delete_migrations():
 
     # adding gyms to db
     print 'Adding gyms to db...'
-    gd.create_gym("Helen Newman", is_gym=True)
-    gd.create_gym("Teagle Up", is_gym=True)
-    gd.create_gym("Teagle Down", is_gym=True)
-    gd.create_gym("Noyes", is_gym=True)
-    gd.create_gym("Appel", is_gym=True)
+    _, helen_newman = gd.create_gym("Helen Newman", is_gym=True)
+    _, teagle_up = gd.create_gym("Teagle Up", is_gym=True)
+    _, teagle_down = gd.create_gym("Teagle Down", is_gym=True)
+    _, noyes = gd.create_gym("Noyes", is_gym=True)
+    _, appel = gd.create_gym("Appel", is_gym=True)
+
+    # adding gym_hours to db
+    print 'Adding gym_hours to db...'
+    # helen_newman
+    ghd.create_gym_hour(helen_newman.id, 0, dt.time(10), dt.time(23, 30))
+    for n in range(1, 5):
+        ghd.create_gym_hour(helen_newman.id, n, dt.time(6), dt.time(23, 30))
+    ghd.create_gym_hour(helen_newman.id, 6, dt.time(10), dt.time(22))
+    # teagle_up
+    ghd.create_gym_hour(teagle_up.id, 0, dt.time(12), dt.time(17, 45))
+    for n in range(1, 4):
+        ghd.create_gym_hour(teagle_up.id, n, dt.time(7), dt.time(22, 45))
+    ghd.create_gym_hour(teagle_up.id, 5, dt.time(7), dt.time(20))
+    ghd.create_gym_hour(teagle_up.id, 6, dt.time(12), dt.time(17, 45))
+    # teagle_down
+    ghd.create_gym_hour(teagle_down.id, 0, dt.time(12), dt.time(17, 45))
+    for n in range(1, 4):
+        ghd.create_gym_hour(teagle_down.id, n, dt.time(7), dt.time(22, 45))
+    ghd.create_gym_hour(teagle_down.id, 5, dt.time(7), dt.time(20))
+    ghd.create_gym_hour(teagle_down.id, 6, dt.time(12), dt.time(17, 45))
+    # noyes
+    ghd.create_gym_hour(noyes.id, 0, dt.time(11, 30), dt.time(23, 30))
+    for n in range(1, 5):
+        ghd.create_gym_hour(noyes.id, n, dt.time(7), dt.time(23, 30))
+    ghd.create_gym_hour(noyes.id, 6, dt.time(11, 30), dt.time(22))
+    # appel
+    ghd.create_gym_hour(appel.id, 0, dt.time(9), dt.time(13))
+    for n in range(1, 5):
+        ghd.create_gym_hour(appel.id, n, dt.time(15), dt.time(23, 30))
+    ghd.create_gym_hour(appel.id, 6, dt.time(9), dt.time(13))
+
 
   except OSError:
     os.chdir('scripts')
