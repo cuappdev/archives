696b2522e9200b95a6fc3c0ec265c70c593db1f6
williambaibai <williambaibai@gmail.com>
2018-04-09 16:44:21 -0400

    minor issue in PR

--- src/app/dao/gymclassinstance_dao.py
+++ src/app/dao/gymclassinstance_dao.py
@@ -50,8 +50,9 @@ def get_gym_class_instances_by_time(time):
   ).all()
 
 def get_gym_class_instances_by_date(date):
-  """Takes as input a string formatted date: MM/DD/YYYY
-   Example: 03/18/2018 is March 18th, 2018"""
+  """Takes a string formatted date as input: MM/DD/YYYY
+  Example: 03/18/2018 is March 18th, 2018
+  """
   input_date = datetime.strpdate(date, '%m/%d/%Y').date()
   return GymClassInstance.query.filter(
       GymClassInstance.start_dt.date() == input_date
