d46c02155ad77cb50d77a1c4f93c0d8bb171fdca
williambaibai <williambaibai@gmail.com>
2018-04-15 14:26:31 -0400

    instructors_route

--- src/app/controllers/get_all_instructors.py
+++ src/app/controllers/get_all_instructors.py
@@ -9,4 +9,18 @@ class GetAllInstructorsController(AppDevController):
 
   def content(self, **kwargs):
     instructors = instructors_dao.get_all_instructors()
-    return [(instructor.id, instructor.name) for instructor in instructors]
+    result = []
+    for instructor in instructors:
+        serialized_instructor = instructor_schema.dump(instructor).data
+
+        instructor_class = gymclass_dao.get_gym_classes_by_instructor(
+                    instructor.id
+        )
+        class_descs = [class_descs_dao.get_class_desc_by_id(inc.id)
+                       for inc in instructor_class]
+        gymclasses = [class_desc_schema.dump(gymclass).data
+                      for gymclass in class_descs]
+
+        serialized_instructor["classes"] = gymclasses
+        result.append(serialized_instructor)
+    return result
