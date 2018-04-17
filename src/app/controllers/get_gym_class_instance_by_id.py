7cf62f6eb76a7729c539c13755f1287c5e2a1041
Young Kim <young-k@protonmail.com>
2018-04-17 16:16:04 -0400

    fixed get_by_id fxn

--- src/app/controllers/get_gym_class_instance_by_id.py
+++ src/app/controllers/get_gym_class_instance_by_id.py
@@ -9,29 +9,27 @@ class GetGymClassInstanceByIdController(AppDevController):
     return ['GET']
 
   def content(self, **kwargs):
-    gymclass_instance_id = request.view_args['id']
     gymclass_instance = gymclassinstance_dao.get_gym_class_instance_by_id(
-            gymclass_instance_id
+        request.view_args['id']
     )
-    serialized_gym = {"id": gymclass_instance_id}
+    serialized_gym = {"id": gymclass_instance.id}
 
-    # get gymclass
     gym_class = gymclass_dao.get_gym_class_by_id(
-            gymclass_instance.gym_class_id
+        gymclass_instance.gym_class_id
     )
 
     # get instructor
     instructor = instructors_dao.get_instructor_by_id(
-            gymclass.instructor_id
+        gym_class.instructor_id
     )
     instructor = instructor_schema.dump(instructor).data
     serialized_gym["instructor"] = instructor
 
     # get class_desc
     class_desc = class_descs_dao.get_class_desc_by_id(
-            gym_class.class_desc_id
+        gym_class.class_desc_id
     )
     class_desc = class_desc_schema.dump(class_desc).data
-    serialized_gym["gym_class"] = gymclass
+    serialized_gym["class_desc"] = class_desc
 
     return serialized_gym
