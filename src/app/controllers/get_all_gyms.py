9f0d8cb316b78de88bc5107dcfb0645a33604e76
Young Kim <young-k@protonmail.com>
2018-04-17 13:52:55 -0400

    fix to get_all_gyms route

--- src/app/controllers/get_all_gyms.py
+++ src/app/controllers/get_all_gyms.py
@@ -17,14 +17,15 @@ class GetAllGymsController(AppDevController):
         gym_hours = gymhours_dao.get_gym_hour({'gym_id' : gym.id})
         serialized_gym_hours = []
         for gh in gym_hours:
-            serialized_gh = gym_hour_schema.dump(gh)
-            open_time = serialized_gh[0]['open_time']
-            close_time = serialized_gh[0]['close_time']
-            serialized_gh[0]['open_time'] = \
+            serialized_gh = gym_hour_schema.dump(gh)[0]
+            open_time = serialized_gh['open_time']
+            close_time = serialized_gh['close_time']
+            serialized_gh['open_time'] = \
                 (dt.strptime(open_time, '%H:%M:%S')).strftime('%I:%M%p')
-            serialized_gh[0]['close_time'] = \
+            serialized_gh['close_time'] = \
                 (dt.strptime(close_time, '%H:%M:%S')).strftime('%I:%M%p')
             serialized_gym_hours.append(serialized_gh)
+
         serialized_gym['gym_hours'] = serialized_gym_hours
         populartimeslist = \
             populartimeslist_dao.get_populartimeslist_by_gym(gym.id)
