2e70b550f2959894d0b1d4a429cdd292a46d6d45
williambaibai <williambaibai@gmail.com>
2018-04-17 21:59:22 -0400

    added fk to gyms

--- src/app/models/gym.py
+++ src/app/models/gym.py
@@ -6,11 +6,15 @@ class Gym(Base):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(255), nullable=False, unique=True)
   equipment = db.Column(db.String(1500))
-  location = db.Column(db.String(500))
   is_gym = db.Column(db.Boolean, nullable=False, default=False)
+  location_gym_id = db.Column(
+      db.Integer,
+      db.ForeignKey('gyms.id', ondelete='CASCADE')
+  )
+  location_gym = db.relationship('Gym', remote_side=[id])
 
   def __init__(self, **kwargs):
     self.equipment = kwargs.get('equipment')
-    self.location = kwargs.get('location')
     self.is_gym = kwargs.get('is_gym')
     self.name = kwargs.get('name')
+    self.location_id = kwargs.get('location_gym_id')
