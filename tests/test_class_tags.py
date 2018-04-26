74c4a65eed70fd7158ee88935ffa4cd0f4842bab
williambaibai <williambaibai@gmail.com>
2018-04-25 22:46:30 -0400

    test

--- tests/test_class_tags.py
+++ tests/test_class_tags.py
@@ -8,6 +8,3 @@ class ClassTagsTestCase(TestCase):
     self.assertIn(test_tag, ctd.get_all_tags())
     self.assertEqual(test_tag, ctd.get_class_tag_by_name("test1"))
     self.assertEqual(test_tag, ctd.get_class_tag_by_id(test_tag.id))
-    test_class_desc = cdd.get_class_desc_by_id(2)
-    test_tag.class_descs.append(test_class_desc)
-    self.assertIn(test_class_desc, ctd.get_class_descs_by_tag([test_tag.id]))
