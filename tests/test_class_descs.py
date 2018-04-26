cf3861fab58a1df0a23897c53c74a1910b583622
sameerkhoja <sk2327@cornell.edu>
2018-04-25 22:36:31 -0400

    added test case

--- tests/test_class_descs.py
+++ tests/test_class_descs.py
@@ -2,10 +2,12 @@ from tests.test_case import *
 from app.dao import class_descs_dao as cdd
 
 class ClassDescsTestCase(TestCase):
-  def test_classdesc_methods(self):
+  def test_classdescs_methods(self):
     test_cdd1 = cdd.create_class_desc('test_name', 'test_desc')[1]
     test_cdd2 = cdd.create_class_desc('test_name2', 'test_desc2')[1]
     self.assertEquals(test_cdd1, cdd.get_class_desc_by_name('test_name'))
     self.assertEquals(test_cdd2, cdd.get_class_desc_by_name('test_name2'))
     self.assertEquals(test_cdd1, cdd.get_class_desc_by_id(test_cdd1.id))
     self.assertEquals(test_cdd2, cdd.get_class_desc_by_id(test_cdd2.id))
+    self.assertIn(test_cdd1, cdd.get_all_class_descs())
+    self.assertIn(test_cdd2, cdd.get_all_class_descs())
